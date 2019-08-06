15befd32c8ef70f6d3dc6cc1a7843deede564f9b
John Letey <30328854+johnletey@users.noreply.github.com>
2019-08-06 14:28:00 +0100

    Add ability to print out users

--- api/server.go
+++ api/server.go
@@ -22,9 +22,12 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 	return cv.validator.Struct(i)
 }
 
+var users []*User
+
 func main() {
 	e := echo.New()
 	e.Validator = &CustomValidator{validator: validator.New()}
+	e.GET("/users", getUsers)
 	e.POST("/user", func(c echo.Context) (err error) {
 		u := new(User)
 		if err = c.Bind(u); err != nil {
@@ -33,8 +36,14 @@ func main() {
 		if err = c.Validate(u); err != nil {
 			return
 		}
+		users = append(users, u)
 		return c.JSON(http.StatusCreated, u)
 	})
 	e.HideBanner = true
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET("/users", getUsers)
+func getUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
 }
