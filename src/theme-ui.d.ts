77efeee4cf95890bb3b12a5d775f37d2d56d8ac4
Fran González <fgonzaleva@icloud.com>
2019-08-13 20:58:45 +0200

    Upgraded to new theme-ui types and finished TypeScript port

--- src/theme-ui.d.ts
+++
@@ -1,8 +0,0 @@
-import * as themeUi from 'theme-ui'
-const _ColorMode = require('theme-ui').ColorMode
-const _InitializeColorMode = require('theme-ui').InitializeColorMode
-
-declare module 'theme-ui' {
-    export const ColorMode = _ColorMode
-    export const InitializeColorMode = _InitializeColorMode
-}
