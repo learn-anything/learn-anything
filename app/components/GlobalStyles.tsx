6951233793c1f80429bf69d3ea9f4d52cf36362e
John Letey <30328854+johnletey@users.noreply.github.com>
2020-03-06 15:14:22 +0000

    System dependent light/dark mode

--- app/components/GlobalStyles.tsx
+++ app/components/GlobalStyles.tsx
@@ -8,8 +8,22 @@ const GlobalStyles = createGlobalStyle`
   //   html { font-family: 'Inter var', sans-serif; }
   // }
   
-  html {
+  :root {
+    --text: 0, 0, 0;
+    --background: 255, 255, 255;
+  }
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --text: 255, 255, 255;
+      --background: 0, 0, 0;
+    }
+  }
+
+  html,
+  body {
     font-family: 'Jost';
+    background-color: rgb(var(--background));
+    color: rgb(var(--text));
   }
 `;
 
