c7c757eaf9279017e45dbb9d59a4839daa954453
John Letey <johnletey@gmail.com>
2020-03-30 16:14:31 +0100

    chakra-ui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,33 @@
 import {MDXProvider} from '@mdx-js/react';
-import Providers from '../components/Providers';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import customTheme from '../components/theme';
+import { DefaultSeo } from 'next-seo';
 
 const mdComponents = {
   h1: props => <h1 style={{color: 'tomato'}} {...props} />
 }
 
+const SeoConfig = {
+  title: 'Learn Anything',
+  description: '',
+  openGraph: {
+    // TODO
+  },
+  twitter: {
+    handle: '@learnanything_',
+    site: '@learnanything_',
+    cardType: 'summary_large_image'
+  }
+};
+
 export default ({Component, pageProps}) => (
-  <MDXProvider components={mdComponents}>
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
-  </MDXProvider>
+  <>
+    <DefaultSeo {...SeoConfig} />
+    <MDXProvider components={mdComponents}>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </MDXProvider>
+  </>
 )
