582b8ce0229a8392df37cc579f5e705d1b1bb697
John Letey <johnletey@gmail.com>
2020-03-31 18:30:28 +0100

    search

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import {
+  Stack,
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Icon,
+} from "@chakra-ui/core";
 import Container from "../components/Container";
 
 const IndexPage = () => {
-  return <Container>{}</Container>;
+  return (
+    <Container>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <InputGroup>
+            <InputLeftElement children={<Icon name="search" />} />
+            <Input type="search" placeholder="Search topics" />
+          </InputGroup>
+        </Flex>
+      </Stack>
+    </Container>
+  );
 };
 
 export default IndexPage;
--- app/src/theme.tsx
+++ app/src/theme.tsx
@@ -2,4 +2,21 @@ import { theme } from "@chakra-ui/core";
 
 export default {
   ...theme,
+  icons: {
+    ...theme.icons,
+    search: {
+      path: (
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <line x1="21" y1="21" x2="16.65" y2="16.65" />
+        </g>
+      ),
+    },
+  },
 };
