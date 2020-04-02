909ee92816aea0dd58c8afec70dc4ade9dfbc3e5
John Letey <johnletey@gmail.com>
2020-04-02 19:35:40 +0100

    tag support in new guide page

--- app/src/pages/new.tsx
+++ app/src/pages/new.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import { Stack, Flex, Heading } from "@chakra-ui/core";
+import React, { useRef, useState } from "react";
+import {
+  Stack,
+  Flex,
+  Heading,
+  InputGroup,
+  Input,
+  InputRightElement,
+  Button,
+  Tag,
+} from "@chakra-ui/core";
 import Container from "../components/Container";
 
 const NewGuidePage = () => {
+  const inputEl = useRef(null);
+  const [state, setState] = useState({
+    tags: [],
+  });
+
+  const updateTags = async () => {
+    const input = inputEl.current.value;
+    if (!(input == "")) {
+      if (!state.tags.find((tag) => tag === input)) {
+        setState({ tags: [...state.tags, input] });
+      }
+      inputEl.current.value = "";
+    }
+  };
+
   return (
     <Container>
       <Stack
@@ -20,6 +44,24 @@ const NewGuidePage = () => {
           w="100%"
         >
           <Heading letterSpacing="tight">Create a new guide</Heading>
+          <Stack isInline mt={4} w="100%">
+            <Input
+              aria-label="Topics for guide"
+              placeholder="Enter a topic"
+              ref={inputEl}
+              type="text"
+            />
+            <Button fontWeight="bold" size="md" onClick={updateTags}>
+              Add
+            </Button>
+          </Stack>
+          <Stack isInline mt={4}>
+            {state.tags.map((tag) => (
+              <Tag variant="outline" size="lg">
+                {tag}
+              </Tag>
+            ))}
+          </Stack>
         </Flex>
       </Stack>
     </Container>
