da91c7669eefbe92c8a8e72e72aca8b19a61903a
John Letey <johnletey@gmail.com>
2020-09-19 15:58:10 +0100

    feat: properly add link

--- web/app/auth/mutations/addLink.ts
+++ web/app/auth/mutations/addLink.ts
@@ -1,11 +1,15 @@
 import { SessionContext } from "blitz"
 import db from "db"
 
-export default async function addLink({url, title}: {url: string, title: string}, ctx: { session?: SessionContext} = {}) {
-  await db.link.create({ data: {
-    title,
-    url,
-    user: ctx && ctx.session && ctx.session.userId
-   }})
-   return
+export default async function addLink(
+  { url, title }: { url: string; title: string },
+  ctx: { session?: SessionContext } = {}
+) {
+  await db.link.create({
+    data: {
+      title,
+      url,
+      user: { connect: { id: ctx.session!.userId } },
+    },
+  })
 }
