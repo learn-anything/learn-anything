373f926e0557c67657a6cd10696b8d8524e63ce9
Nikita Voloboev <nikita.voloboev@gmail.com>
2020-09-19 15:43:07 +0200

    .

--- web/app/pages/index.tsx
+++ web/app/pages/index.tsx
@@ -2,7 +2,7 @@ import logout from "app/auth/mutations/logout"
 import { useCurrentUser } from "app/hooks/useCurrentUser"
 import Layout from "app/layouts/Layout"
 import { BlitzPage, Link } from "blitz"
-import React from "react"
+import React, { Suspense } from "react"
 
 /*
  * This file is just for a pleasant getting started page for your new app.
@@ -11,7 +11,6 @@ import React from "react"
 
 const UserInfo = () => {
   const currentUser = useCurrentUser()
-  console.log(currentUser)
 
   if (currentUser) {
     return (
@@ -49,182 +48,67 @@ const UserInfo = () => {
   }
 }
 
-const SignUp = () => {
-  return (
-    <>
-      <Link href="/signup">
-        <a className="button small">
-          <strong>Sign Up</strong>
-        </a>
-      </Link>
-      <Link href="/login">
-        <a className="button small">
-          <strong>Login</strong>
-        </a>
-      </Link>
-    </>
-  )
-}
-
 const AddLink = () => {
+  const currentUser = useCurrentUser()
+  const [url, setUrl] = React.useState("")
+  const [title, setTitle] = React.useState("")
   return (
     <div>
-      <span>Add Link</span>
-      <span>Title</span>
-      <input type="text" />
-      <span>URL</span>
-      <input type="text" />
+      {currentUser && (
+        <form>
+          <h1 className="font-bold">Add Link</h1>
+          <div>
+            <span className="mr-2">Title</span>
+            <input
+              className="bg-gray-200"
+              type="text"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value)
+              }}
+            />
+          </div>
+          <div>
+            <span className="mr-2">URL</span>
+            <input
+              className="bg-gray-200"
+              type="text"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value)
+              }}
+            />
+          </div>
+          <button
+            className="bg-gray-200 mt-2"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault()
+              console.log(url)
+              console.log(title)
+            }}
+          >
+            Add
+          </button>
+        </form>
+      )}
     </div>
   )
 }
 
 const Home: BlitzPage = () => {
-  const currentUser = useCurrentUser()
   return (
     <div className="container">
       <main>
-        <span>Hi {currentUser && currentUser.name}</span>
-        {currentUser ? <AddLink /> : <SignUp />}
-        {/* <div className="buttons" style={{ marginTop: "1rem", marginBottom: "5rem" }}>
-          <Suspense fallback="Loading...">
-            <UserInfo />
-          </Suspense>
-        </div> */}
+        <Suspense fallback="Loading...">
+          <UserInfo />
+        </Suspense>
+        <Suspense fallback="Loading...">
+          <AddLink />
+        </Suspense>
       </main>
 
       <footer></footer>
-
-      {/* <style jsx global>{`
-        @import url("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;700&display=swap");
-
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: "Libre Franklin", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-          box-sizing: border-box;
-        }
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main p {
-          font-size: 1.2rem;
-        }
-
-        p {
-          text-align: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 60px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #45009d;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer a {
-          color: #f4f4f4;
-          text-decoration: none;
-        }
-
-        .logo {
-          margin-bottom: 2rem;
-        }
-
-        .logo img {
-          width: 300px;
-        }
-
-        .buttons {
-          display: grid;
-          grid-auto-flow: column;
-          grid-gap: 0.5rem;
-        }
-        .button {
-          font-size: 1rem;
-          background-color: #6700eb;
-          padding: 1rem 2rem;
-          color: #f4f4f4;
-          text-align: center;
-        }
-
-        .button.small {
-          padding: 0.5rem 1rem;
-        }
-
-        .button:hover {
-          background-color: #45009d;
-        }
-
-        .button-outline {
-          border: 2px solid #6700eb;
-          padding: 1rem 2rem;
-          color: #6700eb;
-          text-align: center;
-        }
-
-        .button-outline:hover {
-          border-color: #45009d;
-          color: #45009d;
-        }
-
-        pre {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-        }
-        code {
-          font-size: 0.9rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style> */}
     </div>
   )
 }
---
+++ web/app/queries/links.ts
@@ -0,0 +1,21 @@
+// app/products/queries/getProduct.tsx
+
+// type GetProjectInput = {
+//   where: FindOneProjectArgs["where"]
+// }
+
+// return all links from a user
+export async function getLinks() {
+  // const project = await debug.proj
+  // Can do any processing, fetching from other APIs, etc
+  // const project = await db.project.findOne({where})
+  // return project
+}
+
+// save a link
+export async function saveLink() {
+  // const project = await debug.proj
+  // Can do any processing, fetching from other APIs, etc
+  // const project = await db.project.findOne({where})
+  // return project
+}
