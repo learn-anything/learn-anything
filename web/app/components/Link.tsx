c05041b10ed03dfbf6e39f3629b369e1d282a17b
John Letey <johnletey@gmail.com>
2020-09-20 11:23:10 +0100

    .

--- web/app/components/Link.tsx
+++ web/app/components/Link.tsx
@@ -1,17 +1,19 @@
-import { Flex } from "@chakra-ui/core"
+import { Box, Text } from "@chakra-ui/core"
 
 // flag to show full, only host url
 export const Link = ({ link }) => {
   return (
-    <Flex mb={1} overflow="hidden" maxW="sm" p={1} borderWidth="1px" border="text-gray-600">
-      <span>{link.title}</span>
-      <span className="text-gray-600">{link.url}</span>
-    </Flex>
+    <Box
+      mb={1}
+      overflow="hidden"
+      maxW="sm"
+      p={1}
+      rounded="md"
+      borderWidth="2px"
+      border="text-gray-600"
+    >
+      <Text>{link.title}</Text>
+      <Text className="text-gray-600">{link.url}</Text>
+    </Box>
   )
-}
-
-{
-  /* <Box bg="tomato" w="100%" p={4} color="white">
-  This is the Box
-</Box> */
 }
