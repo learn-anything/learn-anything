ff6d22436f7d42651e4090b6a13d06a0e738e4ef
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-06-10 22:12:49 +0200

    .

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,29 +1,7 @@
-import { Link } from "solid-app-router"
-
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <Link
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </Link>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <Link href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </Link>{" "}
-      </p>
+    <main>
+      <div>Hello new LA ✨</div>
     </main>
   )
 }
