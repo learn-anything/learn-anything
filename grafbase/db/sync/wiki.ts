e6f0129ba3d227ac7a0668b1f2d8bb3e5876f92f
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-06-30 18:18:35 +0200

    .

--- grafbase/db/sync/wiki.ts
+++ grafbase/db/sync/wiki.ts
@@ -1,35 +1,68 @@
-import { readFile, readdir } from "fs/promises"
-import { basename, dirname, extname, join } from "path"
+import * as fs from "fs"
+import { readFile } from "fs/promises"
+import * as path from "path"
+import { basename, dirname } from "path"
 
-export function syncWiki() {}
+export async function syncWiki() {}
 
 // overwrite topics on server with local files
-export function forceWikiSync() {
+export async function forceWikiSync() {
   // createTopic(
   //   { name: "Physics", content: "Physics is fun" },
   //   "4ac43a0c-169f-11ee-93a2-9bad7dd0cab0"
   // )
   let fileIgnoreList = ["readme.md"]
-  topicsFromFolder(fileIgnoreList)
-}
-
-async function topicsFromFolder(ignoreList?: string[]) {}
-
-async function seedWikiFromFolder(directoryPath: string) {
-  const entries = await readdir(directoryPath, { withFileTypes: true })
-
-  for (const entry of entries) {
-    const entryPath = join(directoryPath, entry.name)
-    if (entry.isDirectory()) {
-      await seedWikiFromFolder(entryPath)
-    } else if (entry.isFile() && extname(entry.name) === ".md") {
-      await processFile(entryPath)
-    }
+  const files = await markdownFilePaths(
+    "/Users/nikiv/src/docs/wiki/docs",
+    fileIgnoreList
+  )
+  if (files.length > 0) {
+    processFile(files[0])
   }
 }
 
+async function markdownFilePaths(
+  directoryPath: string,
+  ignoreList: string[] = []
+): Promise<string[]> {
+  let filesToProcess: string[] = []
+  const entries = fs.readdirSync(directoryPath, { withFileTypes: true })
+
+  for (let entry of entries) {
+    const fullPath = path.join(directoryPath, entry.name)
+
+    if (entry.isDirectory()) {
+      const subDirFiles = await markdownFilePaths(fullPath, ignoreList)
+      filesToProcess = [...filesToProcess, ...subDirFiles]
+    } else if (
+      entry.isFile() &&
+      path.extname(entry.name) === ".md" &&
+      !ignoreList.includes(entry.name.toLowerCase())
+    ) {
+      filesToProcess.push(fullPath)
+    }
+  }
+  return filesToProcess
+}
+
+// if its index.md, take name of parent folder
+
+// async function processFileAsTopic(filePath: string) {
+//   const entries = await readdir(directoryPath, { withFileTypes: true })
+
+//   for (const entry of entries) {
+//     const entryPath = join(directoryPath, entry.name)
+//     if (entry.isDirectory()) {
+//       await seedWikiFromFolder(entryPath)
+//     } else if (entry.isFile() && extname(entry.name) === ".md") {
+//       await processFile(entryPath)
+//     }
+//   }
+// }
+
 async function processFile(filePath: string) {
   const data = (await readFile(filePath)).toString()
+  console.log(data)
 
   // Extract the title from the frontmatter
   const frontmatterMatch = data.match(
