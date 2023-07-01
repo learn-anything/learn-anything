2a0d014df567e6c08bba7cc2a74b283405efcf4e
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-07-01 21:32:26 +0200

    .

--- grafbase/resolvers/topics/get.ts
+++ grafbase/resolvers/topics/get.ts
@@ -1,20 +1,6 @@
 // import * as edgedb from "edgedb"
 
-// TODO: use auth token to get user
-// get topics for a given user
+// get all topics for a given user
 export default async function GetTopicsResolver() {
-  // const conn = await edgedb.connect({
-  //   user: "<edgedb-cloud-username>",
-  //   host: "<edgedb-cloud-host>",
-  //   port: "<edgedb-cloud-port>",
-  //   database: "<edgedb-cloud-database>",
-  //   password: "<edgedb-cloud-password>",
-  // });
-
-  // try {
-  //   console.log(await conn.queryOne("SELECT 1;"));
-  // } finally {
-  //   await conn.close();
-  // }
   return { topic: "Physics" }
 }
