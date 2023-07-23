bde0d587904319744db84b2f476a4a7b451088ec
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-07-23 14:03:33 +0200

    .

--- app/tests/wiki.test.ts
+++ app/tests/wiki.test.ts
@@ -19,31 +19,35 @@ function parseMarkdownFile(filePath: string) {
   console.log(filePath, "file path")
 }
 
-function getWikiFolderPath() {
+function getWikiFolderPath(folderName: string) {
   let fileDirectoryPath = __dirname
-  return fileDirectoryPath.replace("/app/tests", "/seed/wiki/nikita")
+  return fileDirectoryPath.replace("/app/tests", `/seed/wiki/${folderName}`)
 }
 
-test("parse markdown file", async () => {
-  const wikiFolderPath = getWikiFolderPath()
+function getTestFolderPath() {
+  let fileDirectoryPath = __dirname
+  return fileDirectoryPath.replace("/app/tests", "/seed/test")
+}
 
-  const filePaths = await markdownFilePaths(wikiFolderPath)
-  const filePath = filePaths[0]
+test("parse test folder correctly", async () => {
+  const testFolderPath = getTestFolderPath()
+  console.log(testFolderPath)
 
-  const fileContent = (await readFile(filePath)).toString()
-
-  const tree = fromMarkdown(fileContent)
-
-  for (const node of tree.children) {
-    console.log(toString(node), "node")
-  }
-
-  // TODO: does not save to sqlite when run with bun at least
-  tinybase.getStore().addRow("wiki", {
-    wikiFolderPath: wikiFolderPath,
-  })
-
-  // expect(wikiFolderPath).toBe(
-  //   "/Users/nikiv/src/app/learn-anything/seed/wiki/nikita"
-  // )
+  // const fileContent = (await readFile(filePath)).toString()
+  // const tree = fromMarkdown(fileContent)
 })
+
+// test("parse Nikita's wiki correctly", async () => {
+//   const wikiFolderPath = getWikiFolderPath("nikita")
+//   const filePaths = await markdownFilePaths(wikiFolderPath)
+//   const filePath = filePaths[0]
+//   const fileContent = (await readFile(filePath)).toString()
+//   const tree = fromMarkdown(fileContent)
+//   for (const node of tree.children) {
+//     console.log(toString(node), "node")
+//   }
+//   // TODO: does not save to sqlite when run with bun at least
+//   tinybase.getStore().addRow("wiki", {
+//     wikiFolderPath: wikiFolderPath,
+//   })
+// })
