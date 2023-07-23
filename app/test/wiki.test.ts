639a836fb5d40667eb9d39c9ca2167c73faca861
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-07-23 02:15:56 +0200

    .

--- app/test/wiki.test.ts
+++ app/test/wiki.test.ts
@@ -1,5 +1,7 @@
 import { setupTinybaseStore } from "~/packages/preload/src/tinybase/tinybase"
 import { markdownFilePaths } from "~/packages/preload/src/tinybase/wiki"
+// TODO: no idea why this errors
+// @ts-ignore
 import { expect, test } from "bun:test"
 
 // this file is ran via `pnpm app:test`
@@ -11,28 +13,18 @@ import { expect, test } from "bun:test"
 const tinybase = setupTinybaseStore()
 const store = tinybase.getStore()
 
-// similar function to syncWikiFromSeed in preload/index.ts
-async function syncWikiFromSeed() {
-  let fileDirectoryPath = __dirname
-  const wikiFolderPath = fileDirectoryPath.replace(
-    "/app/scripts",
-    "/seed/wiki/nikita"
-  )
-  const filePaths = await markdownFilePaths(wikiFolderPath)
-
-  parseMarkdownFile(filePaths[0])
-}
-
 function parseMarkdownFile(filePath: string) {
   console.log(filePath, "file path")
 }
 
-async function main() {
-  await syncWikiFromSeed()
+function getWikiFolderPath() {
+  let fileDirectoryPath = __dirname
+  return fileDirectoryPath.replace("/app/test", "/seed/wiki/nikita")
 }
 
-main()
-
-test("2 + 2", () => {
-  expect(2 + 2).toBe(4)
+test("parse markdown file", () => {
+  const wikiFolderPath = getWikiFolderPath()
+  expect(wikiFolderPath).toBe(
+    "/Users/nikiv/src/app/learn-anything/seed/wiki/nikita"
+  )
 })
