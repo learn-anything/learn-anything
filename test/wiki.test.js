07763a8a2c762448b7eaa92d0f597d3c51a1b173
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-07-23 15:21:36 +0200

    .

--- test/wiki.test.js
+++ test/wiki.test.js
@@ -1,5 +1,6 @@
 import { setupTinybaseStore } from "~/packages/preload/src/tinybase/tinybase"
 import { describe } from "fava"
+import { assert, describe, expect, it } from "vitest"
 
 const tinybase = setupTinybaseStore()
 const store = tinybase.getStore()
@@ -13,20 +14,20 @@ describe("parses", () => {
     // const tree = fromMarkdown(fileContent)
   })
 
-  it("Nikita wiki", (t) => {
-    //   const wikiFolderPath = getWikiFolderPath("nikita")
-    //   const filePaths = await markdownFilePaths(wikiFolderPath)
-    //   const filePath = filePaths[0]
-    //   const fileContent = (await readFile(filePath)).toString()
-    //   const tree = fromMarkdown(fileContent)
-    //   for (const node of tree.children) {
-    //     console.log(toString(node), "node")
-    //   }
-    //   // TODO: does not save to sqlite when run with bun at least
-    //   tinybase.getStore().addRow("wiki", {
-    //     wikiFolderPath: wikiFolderPath,
-    //   })
-  })
+  // it("Nikita wiki", (t) => {
+  //   //   const wikiFolderPath = getWikiFolderPath("nikita")
+  //   //   const filePaths = await markdownFilePaths(wikiFolderPath)
+  //   //   const filePath = filePaths[0]
+  //   //   const fileContent = (await readFile(filePath)).toString()
+  //   //   const tree = fromMarkdown(fileContent)
+  //   //   for (const node of tree.children) {
+  //   //     console.log(toString(node), "node")
+  //   //   }
+  //   //   // TODO: does not save to sqlite when run with bun at least
+  //   //   tinybase.getStore().addRow("wiki", {
+  //   //     wikiFolderPath: wikiFolderPath,
+  //   //   })
+  // })
 })
 
 function parseMarkdownFile(filePath: string) {
