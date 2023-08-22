78146b3dff8866334241e5c88466e1dff470cfb0
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-23 00:50:32 +0200

    .

--- app/src-tauri/crates/wiki/src/lib.rs
+++ app/src-tauri/crates/wiki/src/lib.rs
@@ -198,10 +198,11 @@ mod tests {
 
     #[test]
     fn test_no_front_matter_heading_content() {
-        let markdown_string = r#"# Hardware
+        let markdown_string = r#"
+# Hardware
 
-    [Digital Design and Computer Architecture course](https://safari.ethz.ch/digitaltechnik/spring2021/doku.php?id=start), [From Nand to Tetris](https://github.com/ghaiklor/nand-2-tetris) are great.
-    "#;
+[Digital Design and Computer Architecture course](https://safari.ethz.ch/digitaltechnik/spring2021/doku.php?id=start), [From Nand to Tetris](https://github.com/ghaiklor/nand-2-tetris) are great.
+"#;
 
         let topic = parse_md_content_as_topic(&markdown_string).unwrap();
         log!(topic);
@@ -219,36 +220,37 @@ mod tests {
 
     #[test]
     fn test_front_matter_heading_content_notes_links() {
-        let markdown_string = r#"---
-    title: SolidJS
-    ---
+        let markdown_string = r#"
+---
+title: SolidJS
+---
 
-    # [SolidJS](https://www.solidjs.com/)
+# [SolidJS](https://www.solidjs.com/)
 
-    Love Solid. Has [best parts](https://www.youtube.com/watch?v=qB5jK-KeXOs) of [React](react.md).
+Love Solid. Has [best parts](https://www.youtube.com/watch?v=qB5jK-KeXOs) of [React](react.md).
 
-    [Fine grained reactivity](https://dev.to/ryansolid/a-hands-on-introduction-to-fine-grained-reactivity-3ndf) is nice.
+[Fine grained reactivity](https://dev.to/ryansolid/a-hands-on-introduction-to-fine-grained-reactivity-3ndf) is nice.
 
-    ## OSS apps
+## OSS apps
 
-    - [CodeImage](https://github.com/riccardoperra/codeimage)
-    - [Solid Hacker News](https://github.com/solidjs/solid-hackernews)
+- [CodeImage](https://github.com/riccardoperra/codeimage)
+- [Solid Hacker News](https://github.com/solidjs/solid-hackernews)
 
-    ## Notes
+## Notes
 
-    - [Solid will never "re-render" your component/function.](https://twitter.com/Axibord1/status/1606106151539687425)
-        - Means you don't ever have to optimise re-renders.
-        - And don't have to fight with React useEffect.
-    - [Solid Dev Tools](https://github.com/thetarnav/solid-devtools) are great.
-    - createResource makes a signal out of a promise.
-    - Builin components like [For](https://www.solidjs.com/docs/latest/api#for) and [Show](https://www.solidjs.com/docs/latest/api#show) are great.
-    - [Biggest difference between React and Solid is that things that can change are wrapped in signals in Solid, and in dependencies arrays in React.](https://twitter.com/fabiospampinato/status/1528537000504184834)
+- [Solid will never "re-render" your component/function.](https://twitter.com/Axibord1/status/1606106151539687425)
+    - Means you don't ever have to optimise re-renders.
+    - And don't have to fight with React useEffect.
+- [Solid Dev Tools](https://github.com/thetarnav/solid-devtools) are great.
+- createResource makes a signal out of a promise.
+- Builin components like [For](https://www.solidjs.com/docs/latest/api#for) and [Show](https://www.solidjs.com/docs/latest/api#show) are great.
+- [Biggest difference between React and Solid is that things that can change are wrapped in signals in Solid, and in dependencies arrays in React.](https://twitter.com/fabiospampinato/status/1528537000504184834)
 
-    ## Links
+## Links
 
-    - [Hope UI](https://github.com/fabien-ml/hope-ui) - SolidJS component library you've hoped for. ([Docs](https://hope-ui.com/docs/getting-started))
-    - [SolidJS Docs](https://docs.solidjs.com/)
-    "#;
+- [Hope UI](https://github.com/fabien-ml/hope-ui) - SolidJS component library you've hoped for. ([Docs](https://hope-ui.com/docs/getting-started))
+- [SolidJS Docs](https://docs.solidjs.com/)
+"#;
 
         let topic = parse_md_content_as_topic(&markdown_string).unwrap();
         log!(topic);
@@ -318,7 +320,6 @@ Love Solid. Has [best parts](https://www.youtube.com/watch?v=qB5jK-KeXOs) of [Re
 
 - [CodeImage](https://github.com/riccardoperra/codeimage)
 - [Solid Hacker News](https://github.com/solidjs/solid-hackernews)
-
 "#;
 
         assert_eq!(
