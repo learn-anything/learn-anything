c770d421190db4e1a1ce212cfb46b20413d08a32
Kisuyo <ig.intr3st@gmail.com>
2023-08-27 14:56:53 +0200

    .

--- website/src/components/Topic-Components/GuideSummary.tsx
+++ website/src/components/Topic-Components/GuideSummary.tsx
@@ -1,30 +1,57 @@
+import { div } from "edgedb/dist/primitives/bigint"
+import { Show, createSignal } from "solid-js"
+
 export default function GuideSummary() {
+  const [showSummary, setShowSummary] = createSignal(false)
   return (
-    <div class="w-full flex flex-col gap-4 text-[16px] leading-[18.78px]">
-      <div class="flex justify-between items-center ">
-        <div class="font-bold text-[#131313]">GUIDE</div>
-        <div class="text-[#696969]">v1.0</div>
-      </div>
-      <div class="bg-[#FAFAFA] flex flex-col gap-2 rounded-[2px] p-4 w-full">
-        <div class="flex justify-between items-center">
-          <div class="text-[#696969] ">Summary</div>
-          <div class="text-[#3B5CCC]">Expand</div>
+    <>
+      <style>{`
+      #GuideSummaryExpanded {
+        height: 100%;
+
+      }
+      #GuideSummaryMinimised {
+        height: 97px
+      }
+    `}</style>
+      <div class="w-full flex flex-col gap-4 text-[16px] leading-[18.78px]">
+        <div class="flex justify-between items-center ">
+          <div class="font-bold text-[#131313]">GUIDE</div>
+          <div class="text-[#696969]">v1.0</div>
         </div>
-        <div class="text-[#696969] font-light">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem,
-          dolor ratione est iste facilis et accusantium tempore eius, cumque aut
-          voluptate veritatis in qui doloremque. Aspernatur aliquid et vitae
-          sint.
+        <div
+          id={showSummary() ? "GuideSummaryExpanded" : "GuideSummaryMinimised"}
+          class="bg-[#FAFAFA] flex flex-col gap-2 rounded-[2px] p-4 w-full"
+        >
+          <div class="flex justify-between items-center">
+            <div class="text-[#696969] ">Summary</div>
+            <div
+              class="text-[#3B5CCC] cursor-pointer select-none"
+              onClick={() => {
+                setShowSummary(!showSummary())
+              }}
+            >
+              <Show when={showSummary()} fallback={<div>Expand</div>}>
+                Minimise
+              </Show>
+            </div>
+          </div>
+          <div class="text-[#696969] font-light overflow-hidden text-ellipsis">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptatem, dolor ratione est iste facilis et accusantium tempore
+            eius, cumque aut voluptate veritatis in qui doloremque. Aspernatur
+            aliquid et vitae sint.
+          </div>
+        </div>
+        <div class="w-full flex justify-between items-center text-[#696969]">
+          <div>Contents</div>
+          <div class="flex gap-[23px]">
+            <div>Duplicate</div>
+            <div>Improve Guide</div>
+            <div>Filter</div>
+          </div>
         </div>
       </div>
-      <div class="w-full flex justify-between items-center text-[#696969]">
-        <div>Contents</div>
-        <div class="flex gap-[23px]">
-          <div>Duplicate</div>
-          <div>Improve Guide</div>
-          <div>Filter</div>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
--- website/src/components/Topic-Components/TopicNav.tsx
+++ website/src/components/Topic-Components/TopicNav.tsx
@@ -1,25 +1,60 @@
+import { Show, createSignal } from "solid-js"
+
 export default function TopicNav() {
+  const [showInput, setShowInput] = createSignal(false)
   return (
-    <div class="flex flex-col">
-      <div
-        class="h-[80px] w-full p-4 flex items-center justify-between"
-        style={{
-          filter: "drop-shadow(0, 0, 3, rgba(105, 105, 105, 0.32)) !important",
-        }}
-      >
-        <div class="flex gap-4 h-full items-center">
-          <div class=" w-[40px] h-[40px] bg-neutral-200 rounded-full"></div>
-          <div class="w-[212px] rounded-[4px] h-full p-[12px] bg-neutral-200"></div>
-        </div>
-        <div class="flex items-center justify-center gap-4">
-          <div>Themes</div>
-          <div class="bg-[#3B5CCC] leading-[23.48px] w-[91px] text-white h-[39px] flex items-center justify-center font-light text-[20px] px-4 p-2 rounded-full">
-            Log In
+    <>
+      <style>
+        {`
+      #InputMinimised {
+        width: 212px;
+      }
+      #InputExpanded {
+        width: 500px;
+      }
+      `}
+      </style>
+      <div class="flex flex-col">
+        <div
+          class="h-[80px] w-full p-4 flex items-center justify-between"
+          style={{
+            filter:
+              "drop-shadow(0, 0, 3, rgba(105, 105, 105, 0.32)) !important",
+          }}
+        >
+          <div class="flex gap-4 h-full items-center">
+            <div class=" w-[40px] h-[40px] bg-neutral-200 rounded-full"></div>
+            <div
+              onClick={() => {
+                setShowInput(true)
+              }}
+              id={showInput() ? "InputExpanded" : "InputMinimised"}
+              class="bg-neutral-200 rounded-[4px] transition-all h-full p-[12px] text-[#696969] font-light"
+            >
+              <Show
+                when={showInput()}
+                fallback={<div class="">Search Topic</div>}
+              >
+                <div class="flex items-center">
+                  <input
+                    type="text"
+                    class="w-full bg-transparent outline-none"
+                    placeholder="Search Topic"
+                  />
+                </div>
+              </Show>
+            </div>
           </div>
-          <div>Menu</div>
+          <div class="flex items-center justify-center gap-4">
+            <div>Themes</div>
+            <div class="bg-[#3B5CCC] leading-[23.48px] w-[91px] text-white h-[39px] flex items-center justify-center font-light text-[20px] px-4 p-2 rounded-full">
+              Log In
+            </div>
+            <div>Menu</div>
+          </div>
         </div>
+        <div class="h-[30px] w-full bg-[#EAEAEA]">breadcrumb</div>
       </div>
-      <div class="h-[30px] w-full bg-[#EAEAEA]">breadcrumb</div>
-    </div>
+    </>
   )
 }
