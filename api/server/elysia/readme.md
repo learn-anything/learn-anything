3a9091e9e2e510a133fcd0d9247b4431ee08e154
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-29 01:22:20 +0200

    .

---
+++ api/server/elysia/readme.md
@@ -0,0 +1,9 @@
+# Elysia with Bun runtime
+
+## Run
+
+```
+bun run dev
+```
+
+Open http://localhost:3000/ with your browser to see the result.
---
+++ website/lib/graphql.ts
@@ -0,0 +1,39 @@
+import { Mobius } from 'graphql-mobius'
+
+const typeDefs = `
+    type A {
+        A: String!
+        B: String!
+    }
+
+    type Query {
+        Hello(word: String!): A!
+    }
+`
+
+const mobius = new Mobius<typeof typeDefs>({
+    // Using Mobius default fetch client
+    url: 'https://api.saltyaom.com/graphql'
+})
+
+// This is also fine, if you don't care about TypeDefs being available on client-side
+const mobius2 = new Mobius({
+    url: 'https://api.saltyaom.com/graphql'
+    typeDefs
+})
+
+// Call query to execute query
+const result = await mobius.query({
+    Hello: {
+        where: {
+            word: 'Hi'
+        },
+        select: {
+            A: true
+        }
+    }
+})
+
+result
+    .then(x => x?.Hello.A)
+    .then(console.log)
