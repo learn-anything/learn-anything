3c4885fcb8b60af145f92bfa5ea41e2ba7d6192e
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-09-20 16:24:52 +0200

    .

--- grafbase/resolvers/uploadProfilePhoto.ts
+++ grafbase/resolvers/uploadProfilePhoto.ts
@@ -1,13 +1,15 @@
 import { hankoIdFromToken } from "../lib/hanko-validate"
+// import sst from "sst"
 
-export default async function createUserResolver(
+export default async function uploadProfilePhotoResolver(
   root: any,
-  args: { email: string },
+  args: { imageInBase64: string },
   context: any,
 ) {
   const hankoId = await hankoIdFromToken(context)
   if (hankoId) {
     // TODO: use SST to upload a passed in image to S3
     // get URL, then save the URL to the user object
+    // probably image coming in is encoded in base 64, not sure
   }
 }
