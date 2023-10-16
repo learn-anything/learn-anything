3731f1875ef1605d1f07d95c1504aa3e3ca4fb30
Kisuyo <ig.intr3st@gmail.com>
2023-10-16 19:57:29 +0200

    .

--- website/src/components/Icon.tsx
+++ website/src/components/Icon.tsx
@@ -16,6 +16,7 @@ interface Props {
     | "Home"
     | "Heart"
     | "Menu"
+    | "Loader"
   width?: string
   height?: string
   fill?: string
@@ -233,6 +234,27 @@ export default function Icon(props: Props) {
                 stroke-width="1.5"
                 d="M18.25 12L5.75 12"
               ></path>
+            </svg>
+          </Match>
+          <Match when={props.name === "Loader"}>
+            <svg
+              width="24"
+              height="24"
+              stroke="#000"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <style>{`
+          .spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}`}</style>
+              <g class="spinner_V8m1">
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="9.5"
+                  fill="none"
+                  stroke-width="3"
+                ></circle>
+              </g>
             </svg>
           </Match>
           <Match when={props.name === "Delete"}>
