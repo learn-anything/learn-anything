c7f4c447e84cb2af5c57c81dcdde23f67f11a0ee
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-10-16 19:02:13 +0200

    .

--- grafbase/lib/hanko-validate.ts
+++ grafbase/lib/hanko-validate.ts
@@ -1,9 +1,10 @@
 import { jwtVerify, createRemoteJWKSet } from "jose"
 import { GraphQLError } from "graphql"
+import { Context } from "@grafbase/sdk"
 
 // validates that the token in `authorization` header is correct
 // if it is valid, returns hanko id of the user
-export async function hankoIdFromToken(context: any) {
+export async function hankoIdFromToken(context: Context) {
   // when run locally, don't validate the token, return local admin user hanko id
   // if (process.env.LOCAL_USE) {
   //   return process.env.LOCAL_USER_HANKO_ID
@@ -12,6 +13,7 @@ export async function hankoIdFromToken(context: any) {
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new GraphQLError("Missing or invalid Authorization header")
   }
+  console.log(process.env.PUBLIC_HANKO_API_URL, "hanko api url...")
   const JWKS = createRemoteJWKSet(
     new URL(`${process.env.PUBLIC_HANKO_API_URL}/.well-known/jwks.json`)
   )
