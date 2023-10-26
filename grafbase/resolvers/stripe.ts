d6ac06e59cc051676242f0520e0d110ca146db55
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-10-26 16:22:28 +0200

    .

--- grafbase/resolvers/stripe.ts
+++ grafbase/resolvers/stripe.ts
@@ -4,10 +4,10 @@ import Stripe from "stripe"
 import { hankoIdFromToken } from "../lib/hanko-validate"
 import { log, logError } from "../lib/baselime"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-08-16",
-  typescript: true
-})
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2023-08-16",
+//   typescript: true
+// })
 
 type StripePlan = "month" | "year"
 
@@ -17,51 +17,53 @@ export default async function StripeResolver(
   context: Context
 ) {
   log("stripe", "trying to get stripe checkout", { args })
-  const hankoId = await hankoIdFromToken(context)
-  if (hankoId) {
-    try {
-      switch (args.plan) {
-        case "month":
-          const monthSubscription = await stripe.checkout.sessions.create({
-            success_url: process.env.STRIPE_SUCCESS_URL!,
-            mode: "subscription",
-            metadata: {
-              userEmail: args.userEmail
-            },
-            line_items: [
-              {
-                quantity: 1,
-                price: process.env.STRIPE_MONTH_SUBSCRIPTION!
-              }
-            ]
-          })
-          return monthSubscription.url
-        case "year":
-          const yearSubscription = await stripe.checkout.sessions.create({
-            success_url: process.env.STRIPE_SUCCESS_URL!,
-            mode: "subscription",
-            metadata: {
-              userEmail: args.userEmail
-            },
-            line_items: [
-              {
-                quantity: 1,
-                price: process.env.STRIPE_YEAR_SUBSCRIPTION!
-              }
-            ]
-          })
-          return yearSubscription.url
-        default:
-          return {
-            stripeCheckoutUrl: null
-          }
-      }
-    } catch (error) {
-      logError("stripe", error, { args })
-      throw new GraphQLError(JSON.stringify(error))
-    }
-  } else {
-    logError("stripe", "not a member", { args })
-    throw new GraphQLError("not member")
-  }
+  console.log("log test")
+  return "ok"
+  // const hankoId = await hankoIdFromToken(context)
+  // if (hankoId) {
+  //   try {
+  //     switch (args.plan) {
+  //       case "month":
+  //         const monthSubscription = await stripe.checkout.sessions.create({
+  //           success_url: process.env.STRIPE_SUCCESS_URL!,
+  //           mode: "subscription",
+  //           metadata: {
+  //             userEmail: args.userEmail
+  //           },
+  //           line_items: [
+  //             {
+  //               quantity: 1,
+  //               price: process.env.STRIPE_MONTH_SUBSCRIPTION!
+  //             }
+  //           ]
+  //         })
+  //         return monthSubscription.url
+  //       case "year":
+  //         const yearSubscription = await stripe.checkout.sessions.create({
+  //           success_url: process.env.STRIPE_SUCCESS_URL!,
+  //           mode: "subscription",
+  //           metadata: {
+  //             userEmail: args.userEmail
+  //           },
+  //           line_items: [
+  //             {
+  //               quantity: 1,
+  //               price: process.env.STRIPE_YEAR_SUBSCRIPTION!
+  //             }
+  //           ]
+  //         })
+  //         return yearSubscription.url
+  //       default:
+  //         return {
+  //           stripeCheckoutUrl: null
+  //         }
+  //     }
+  //   } catch (error) {
+  //     logError("stripe", error, { args })
+  //     throw new GraphQLError(JSON.stringify(error))
+  //   }
+  // } else {
+  //   logError("stripe", "not a member", { args })
+  //   throw new GraphQLError("not member")
+  // }
 }
