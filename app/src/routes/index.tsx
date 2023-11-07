9b086efde91ca72f6ecd27ee3a5e9422de9bbd55
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-11-07 12:01:54 +0100

    .

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -48,7 +48,7 @@ export default function App() {
             >
               {/* <Editor /> */}
               {/* <TiptapEditor /> */}
-              <CodemirrorEditor />
+              {/* <CodemirrorEditor /> */}
             </Show>
 
             {/* <Show
