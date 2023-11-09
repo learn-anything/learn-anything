ec0cef279f4551a6c4828f0a30d737c3970c1ccd
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-11-09 13:42:37 +0100

    tauri-deep-link

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -13,9 +13,44 @@ use std::ffi::OsStr;
 use std::fs;
 use std::path::PathBuf;
 use tauri::api::dialog::blocking::FileDialogBuilder;
+use tauri::Manager;
 use walkdir::WalkDir;
 
 fn main() {
+    // prepare() checks if it's a single instance and tries to send the args otherwise.
+    // It should always be the first line in your main function (with the exception of loggers or similar)
+    tauri_plugin_deep_link::prepare("xyz.learn-anything");
+    // It's expected to use the identifier from tauri.conf.json
+    // Unfortuenetly getting it is pretty ugly without access to sth that implements `Manager`.
+
+    tauri::Builder::default()
+    .setup(|app| {
+      // If you need macOS support this must be called in .setup() !
+      // Otherwise this could be called right after prepare() but then you don't have access to tauri APIs
+      let handle = app.handle();
+      tauri_plugin_deep_link::register(
+        "my-scheme",
+        move |request| {
+          dbg!(&request);
+          handle.emit_all("scheme-request-received", request).unwrap();
+        },
+      )
+      .unwrap(/* If listening to the scheme is optional for your app, you don't want to unwrap here. */);
+
+      // If you also need the url when the primary instance was started by the custom scheme, you currently have to read it yourself
+      /*
+      #[cfg(not(target_os = "macos"))] // on macos the plugin handles this (macos doesn't use cli args for the url)
+      if let Some(url) = std::env::args().nth(1) {
+        app.emit_all("scheme-request-received", url).unwrap();
+      }
+      */
+
+      Ok(())
+    })
+    // .plugin(tauri_plugin_deep_link::init()) // consider adding a js api later
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             connect_folder,
