e0d55d06bf5fba2e5191e07b2b013d5e50debfb7
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-11-09 14:55:49 +0100

    .

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,25 +1,8 @@
 import solid from "solid-start/vite"
 import { defineConfig } from "vite"
-import devtools from "solid-devtools/vite"
 
-// TODO: ssr: false because with it createResource doesn't log to client
-// because callbacks happen on server
-// something to fix for later
-// ideally the landing page is SSR'd
-// with true, the logs are sent to client
 export default defineConfig({
-  plugins: [
-    solid({ ssr: false }),
-    devtools({
-      /* additional options */
-      autoname: true, // e.g. enable autoname
-      locator: {
-        targetIDE: "vscode-insiders",
-        componentLocation: true,
-        jsxLocation: true,
-      },
-    }),
-  ],
+  plugins: [solid({ ssr: false }) as any],
   clearScreen: false,
   // tauri expects a fixed port, fail if that port is not available
   server: {
