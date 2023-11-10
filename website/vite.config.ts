f831398ca9bc37cb5dccfc17c8d20225d03f7307
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-10 12:14:21 +0100

    Use target: esnext

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "vite"
 import cloudflare from "solid-start-cloudflare-pages"
 
 export default defineConfig({
-  plugins: [solid({ ssr: false, adapter: cloudflare({}) })],
+  build: {
+    target: "esnext"
+  },
+  plugins: [
+    solid({
+      ssr: false,
+      adapter: cloudflare({}) as any
+    }) as any
+  ]
 })
