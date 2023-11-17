4bb38ffa56745bd9e5f5bd11ad1a196e12d2c4f4
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-11-17 22:58:35 +0300

    fix tailwind

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./shared/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
