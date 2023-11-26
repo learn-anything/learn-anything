2ff7a05699b76a754154d1421e1ca88d55ad07f4
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-11-26 14:21:40 +0300

    fix app build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome110" : "safari15",
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
