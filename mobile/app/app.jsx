c933d829b1680bd6d33dc9eb72aad38a33b86300
Kisuyo <ig.intr3st@gmail.com>
2023-12-15 16:04:35 +0100

    add tailwind

--- mobile/app/app.jsx
+++ mobile/app/app.jsx
@@ -1,29 +1,20 @@
-import { createSignal } from 'solid-js';
-import { Component } from './component.jsx';
+import { createSignal } from "solid-js"
+import { Component } from "./component.jsx"
 
 const App = () => {
-  const [count, setCount] = createSignal(0);
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
   return (
     <>
-      <actionbar title="Hello, SolidJS!"></actionbar>
-      <stacklayout>
-        <label>
-          You have tapped {count()} time{count() === 1 ? '' : 's'}
-        </label>
-        {
-          // use 'on:___' instead of 'on___' for event handlers
-          // See https://github.com/SudoMaker/dominative-solid#event-handling for detail
-        }
-        <button class="-primary" on:tap={increment}>
-          Tap me!
-        </button>
-        <Component count={count} />
-      </stacklayout>
+      <Label textWrap="true">
+        <FormattedString>
+          <Span text="This text has a " />
+          <Span text="red " style="color: red" />
+          <Span text="piece of text. " />
+          <Span text="Also, this bit is italic, " fontStyle="italic" />
+          <Span text="and this bit is bold." fontWeight="bold" />
+        </FormattedString>
+      </Label>
     </>
-  );
-};
+  )
+}
 
-export { App };
+export { App }
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+const plugin = require("tailwindcss/plugin")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{css,xml,html,vue,svelte,ts,tsx}"],
+  // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
+  darkMode: ["class", ".ns-dark"],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    /**
+     * A simple inline plugin that adds the ios: and android: variants
+     *
+     * Example usage:
+     *
+     *   <Label class="android:text-red-500 ios:text-blue-500" />
+     *
+     */
+    plugin(function ({ addVariant }) {
+      addVariant("android", ".ns-android &")
+      addVariant("ios", ".ns-ios &")
+    })
+  ],
+  corePlugins: {
+    preflight: false // disables browser-specific resets
+  }
+}
