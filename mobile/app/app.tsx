91d8078ed8709a50b61499aa5fd32a894c0dac0a
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-12-18 13:47:16 +0100

    wip ns

--- mobile/app/app.tsx
+++ mobile/app/app.tsx
@@ -1,13 +1,11 @@
-import { Label, StackLayout } from "@nativescript/core"
-
 const App = () => {
   return (
     <>
-      <StackLayout backgroundColor="#3c495e">
-        <Label text="first" height="70" backgroundColor="#43B3F4" />
-        <Label text="second" height="70" backgroundColor="#1089CA" />
-        <Label text="third" height="70" backgroundColor="#075B88" />
-      </StackLayout>
+      <stacklayout backgroundColor="#3c495e">
+        <label text="testing" height="70" backgroundColor="#43B3F4" />
+        <label text="second" height="70" backgroundColor="#1089CA" />
+        <label text="third" height="70" backgroundColor="#075B88" />
+      </stacklayout>
     </>
   )
 }
