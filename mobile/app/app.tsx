b76689f41b31f3bc201e6d6d8689ed7b36188c0b
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-12-18 12:18:43 +0100

    ns tsconfig

--- mobile/app/app.tsx
+++ mobile/app/app.tsx
@@ -1,18 +1,13 @@
-import { createSignal } from "solid-js"
-import { Component } from "./component.jsx"
+import { Label, StackLayout } from "@nativescript/core"
 
 const App = () => {
   return (
     <>
-      <Label textWrap="true">
-        <FormattedString>
-          <Span text="This text has a " />
-          <Span text="red " style="color: red" />
-          <Span text="piece of text. " />
-          <Span text="Also, this bit is italic, " fontStyle="italic" />
-          <Span text="and this bit is bold." fontWeight="bold" />
-        </FormattedString>
-      </Label>
+      <StackLayout backgroundColor="#3c495e">
+        <Label text="first" height="70" backgroundColor="#43B3F4" />
+        <Label text="second" height="70" backgroundColor="#1089CA" />
+        <Label text="third" height="70" backgroundColor="#075B88" />
+      </StackLayout>
     </>
   )
 }
