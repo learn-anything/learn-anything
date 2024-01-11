9a93639d9eb252a4521012915cd0f3fb742e411d
Nathan Walker <walkerrunpdx@gmail.com>
2024-01-11 05:12:06 -0800

    feat(mobile): touchups to dependencies, setup, types, icons, etc (#93)

--- mobile/app/app.tsx
+++ mobile/app/app.tsx
@@ -1,11 +1,44 @@
+import { CoreTypes, EventData, Image } from "@nativescript/core"
+
 const App = () => {
+  let logo: Image;
+  let pulsating = false;
+  const loadedLogo = (args: EventData) => {
+    logo = args.object as Image
+    if (!pulsating) {
+      pulsating = true;
+      pulseLogo();
+    }
+  }
+  const pulseLogo = () => {
+    logo.animate({
+      scale: { x: 1.2, y: 1.2 },
+      opacity: 1,
+      duration: 1000,
+      iterations: 1,
+      curve: CoreTypes.AnimationCurve.easeInOut
+    }).then(() => {
+      logo.animate({
+        scale: { x: 1, y: 1 },
+        opacity: .8,
+        duration: 800,
+        iterations: 1,
+        curve: CoreTypes.AnimationCurve.easeInOut
+      }).then(() => {
+        pulseLogo();
+      })
+    })
+  }
   return (
     <>
-      <stacklayout backgroundColor="#3c495e">
-        <label text="testing" height="70" backgroundColor="#43B3F4" />
-        <label text="second" height="70" backgroundColor="#1089CA" />
-        <label text="third" height="70" backgroundColor="#075B88" />
-      </stacklayout>
+      <gridlayout rows="auto,*" class="bg-[#3c495e]">
+        <stacklayout class="bg-[#075B88]">
+          <label text="Organize" class="bg-[#43B3F4] h-[70] text-center text-white" />
+          <label text="world's" height="70" class="bg-[#1089CA]  h-[70] text-center text-white" />
+          <label text="knowledge" height="70" class="h-[70] text-center text-white" />
+        </stacklayout>
+        <image row="1" src="~/assets/logo-white-512.png" height="256" width="256" class="mt-8 rounded-2xl" on:loaded={(e: any) => loadedLogo(e)} />
+      </gridlayout>
     </>
   )
 }
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,7 +3,7 @@ const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{css,xml,html,vue,svelte,ts,tsx}"],
+  content: ['./app/**/*.{css,ts,tsx,jsx}'],
   // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
   darkMode: ["class", ".ns-dark"],
   theme: {
