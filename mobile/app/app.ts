2d4028a7dd1ab8f185fc01b62a07e34c74e43ec4
Nikita Voloboev <nikita.voloboev@gmail.com>
2024-01-15 17:45:08 +0300

    .

--- mobile/app/app.ts
+++ mobile/app/app.ts
@@ -1,14 +1,14 @@
 import { Application } from "@nativescript/core"
 import { render } from "@nativescript-community/solid-js"
-import { document } from "dominative";
+import { document } from "dominative"
 import { App } from "./app.tsx"
 
 document.body.actionBarHidden = true
 // todo: check with Ammar on types
 document.body.appendChild(document.createElement("ContentView") as any)
 
-render(App, document.body.firstElementChild);
+render(App, document.body.firstElementChild)
 
-const create = () => document;
+const create = () => document
 
-Application.run({ create });
+Application.run({ create })
--- mobile/app/app.tsx
+++ mobile/app/app.tsx
@@ -1,43 +1,67 @@
 import { CoreTypes, EventData, Image } from "@nativescript/core"
 
 const App = () => {
-  let logo: Image;
-  let pulsating = false;
+  let logo: Image
+  let pulsating = false
   const loadedLogo = (args: EventData) => {
     logo = args.object as Image
     if (!pulsating) {
-      pulsating = true;
-      pulseLogo();
+      pulsating = true
+      pulseLogo()
     }
   }
   const pulseLogo = () => {
-    logo.animate({
-      scale: { x: 1.2, y: 1.2 },
-      opacity: 1,
-      duration: 1000,
-      iterations: 1,
-      curve: CoreTypes.AnimationCurve.easeInOut
-    }).then(() => {
-      logo.animate({
-        scale: { x: 1, y: 1 },
-        opacity: .8,
-        duration: 800,
+    logo
+      .animate({
+        scale: { x: 1.2, y: 1.2 },
+        opacity: 1,
+        duration: 1000,
         iterations: 1,
         curve: CoreTypes.AnimationCurve.easeInOut
-      }).then(() => {
-        pulseLogo();
       })
-    })
+      .then(() => {
+        logo
+          .animate({
+            scale: { x: 1, y: 1 },
+            opacity: 0.8,
+            duration: 800,
+            iterations: 1,
+            curve: CoreTypes.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            pulseLogo()
+          })
+      })
   }
+
+  // draft commit
   return (
     <>
       <gridlayout rows="auto,*" class="bg-[#3c495e]">
         <stacklayout class="bg-[#075B88]">
-          <label text="Organize" class="bg-[#43B3F4] h-[70] text-center text-white" />
-          <label text="world's" height="70" class="bg-[#1089CA]  h-[70] text-center text-white" />
-          <label text="knowledge" height="70" class="h-[70] text-center text-white" />
+          <label
+            text="Organize"
+            class="bg-[#43B3F4] h-[70] text-center text-white"
+          />
+          <label
+            text="world's"
+            height="70"
+            class="bg-[#1089CA]  h-[70] text-center text-white"
+          />
+          <label
+            text="knowledge"
+            height="70"
+            class="h-[70] text-center text-white"
+          />
         </stacklayout>
-        <image row="1" src="~/assets/logo-white-512.png" height="256" width="256" class="mt-8 rounded-2xl" on:loaded={(e: any) => loadedLogo(e)} />
+        <image
+          row="1"
+          src="~/assets/logo-white-512.png"
+          height="256"
+          width="256"
+          class="mt-8 rounded-2xl"
+          on:loaded={(e: any) => loadedLogo(e)}
+        />
       </gridlayout>
     </>
   )
