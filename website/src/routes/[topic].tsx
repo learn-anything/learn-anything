84503a17438659a0bc7aa71f020331c525f3f8d1
Kisuyo <ig.intr3st@gmail.com>
2024-04-01 16:16:19 +0200

    pricing

--- website/src/routes/[topic].tsx
+++ website/src/routes/[topic].tsx
@@ -1,3 +1,89 @@
+import { For, Show, createSignal } from "solid-js"
+import Button from "../../../shared/components/Button"
+import { Motion } from "solid-motionone"
+
 export default function Topic() {
-	return <div>topic</div>
+	return (
+		<div class="ml-[200px] p-3 h-screen">
+			<Sidebar />
+			<div class=" border border-[#191919] rounded-[7px] h-full">
+				<div>
+					<div>Title</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function Sidebar(props: { topics?: string[] }) {
+	const [expanded, setExpanded] = createSignal(false)
+	const [expandTimer, setExpandTimer] = createSignal(false)
+	return (
+		<div class="fixed top-0 left-0 flex justify-between flex-col h-screen min-w-[200px] bg-dark text-textGray">
+			<div class="">
+				<div class="h-[40px] w-[40px] rounded-full bg-white m-[20px]"></div>
+				<div class="col-gap-[8px] pl-2">
+					<div class="w-full [&>*]:p-4 [&>*]:flex [&>*]:items-center [&>*]:px-[11px]">
+						<Button label="My Links" />
+					</div>
+					<div class="">
+						<div class="text-white/20 text-[14px] px-3 p-2">My Topics</div>
+						<For each={props.topics}>
+							{(topic) => {
+								return (
+									<div class="text-white/60 px-3 p-2 cursor-pointer hover:bg-hoverDark rounded-[7px] transition-all">
+										{topic}
+									</div>
+								)
+							}}
+						</For>
+					</div>
+				</div>
+			</div>
+			<div class="relative">
+				<div
+					onClick={() => {
+						setExpanded(!expanded())
+					}}
+					onMouseLeave={() => {
+						setExpandTimer(true)
+						setTimeout(() => {
+							if (expandTimer()) {
+								setExpanded(false)
+							}
+						}, 500)
+					}}
+					onMouseEnter={() => {
+						setExpandTimer(false)
+					}}
+					class="p-4"
+				>
+					Profile
+				</div>
+				<Show when={expanded()}>
+					<Motion.div
+						exit={{ opacity: 0, scale: 0.8 }}
+						animate={{ scale: [0.8, 1.1, 1], opacity: [0, 1] }}
+						transition={{ duration: 0.2, easing: "ease-in" }}
+						class="absolute bottom-12 w-full min-w-fit left-[6px] bg-hoverDark p-1 rounded-[7px]"
+						style={{
+							border: "1px solid #1E1E1E",
+							background: "rgba(55, 55, 55, 0.40)",
+							"backdrop-filter": "blur(8.5px)",
+						}}
+					>
+						<div class="rounded-[7px] h-[34px] cursor-pointer whitespace-nowrap px-[11px] flex items-center hover:bg-softDarkText/10 text-white/60">
+							Settings
+						</div>
+						<div class="rounded-[7px] h-[34px] cursor-pointer whitespace-nowrap px-[11px] flex items-center hover:bg-softDarkText/10 text-white/60">
+							Support
+						</div>
+						<div class="rounded-[7px] h-[34px] cursor-pointer whitespace-nowrap px-[11px] flex items-center hover:bg-softDarkText/10 text-white/60">
+							Sign out
+						</div>
+					</Motion.div>
+				</Show>
+			</div>
+		</div>
+	)
 }
--- website/src/routes/pricing.tsx
+++ website/src/routes/pricing.tsx
@@ -1,3 +1,136 @@
+import { Show, createSignal } from "solid-js"
+import clsx from "clsx"
+
+import Search from "../../../shared/components/Search"
+import Icon from "../../../shared/components/Icon"
+
 export default function Pricing() {
-	return <>pricing</>
+	return (
+		<div
+			class="w-screen h-full"
+			style={{
+				background:
+					"radial-gradient(ellipse 190% 90% at top, rgba(25, 53, 92, 1) 0%, rgba(23, 23, 23, 0.8) 32%)",
+				color: "white",
+
+				display: "flex",
+				"flex-direction": "column",
+				"align-items": "center",
+				"justify-content": "center",
+			}}
+		>
+			<div class="absolute top-5 left-5 cursor-pointer min-w-[50px]">
+				<img src="/logo-black.svg" class="h-[50px] w-[50px]" />
+			</div>
+			<div class="w-full h-full flex flex-col items-center md:p-[90px] p-[14px] py-[40px] gap-[30px]">
+				<div class="flex flex-col gap-2 items-center justify-center">
+					<div
+						class="md:text-[40px] text-[20px] font-bold text-opacity-[0.32] text-white"
+						style={{
+							background: "linear-gradient(#ffffff, #2358E0)",
+
+							"background-clip": "text",
+						}}
+					>
+						Become a member
+					</div>
+					<div class="opacity-40 mb-10 md:text-[16px] text-[14px] text-center px-[26px]">
+						Unlock premium features and help us build the future of education together
+					</div>
+				</div>
+				<Card />
+				<div class="bg-white/5 mt-[80px] md:w-[500px] w-full rounded-[20px] p-[28px] col-gap-[24px]">
+					<div class="flex-center w-full text-[14px] font-bold">Free Account</div>
+					<div class="col-gap-[6px] text-[14px] opacity-40">
+						<div>Free open-source desktop app to edit your notes.</div>
+						<div>See parts of guides available. Explore the topic graph.</div>
+					</div>
+					<div class="w-full flex-center pt-[20px] border-t border-white/5">Your current plan</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function Card() {
+	const [shownPlan, setShownPlan] = createSignal("Monthly")
+	return (
+		<div
+			class="md:w-[500px] w-full p-[2px] rounded-[20px]"
+			style={{
+				background: "linear-gradient(rgba(35, 88, 224, 0.32), rgba(255, 255, 255, 0.32))",
+			}}
+		>
+			<div class="bg-[#171717] p-[20px] col-gap-[28px] rounded-[20px]">
+				<div class="w-full  flex-center flex-col gap-[16px]">
+					<div class="text-[14px] font-bold">Membership</div>
+					<div class="text-[60px] font-bold">
+						$8<span class="text-[14px] font-light opacity-40">/mo</span>
+					</div>
+					<div class="bg-[#252525] rounded-[10px] p-0.5 flex gap-1">
+						<div
+							class={clsx("p-1 px-2 rounded-[10px]", shownPlan() === "Yearly" && "bg-[#171717]")}
+							onClick={() => {
+								setShownPlan("Yearly")
+							}}
+						>
+							Yearly
+						</div>
+						<div
+							class={clsx("p-1 px-2 rounded-[10px]", shownPlan() === "Monthly" && "bg-[#171717]")}
+							onClick={() => {
+								setShownPlan("Monthly")
+							}}
+						>
+							Monthly
+						</div>
+					</div>
+				</div>
+				<div class="col-gap-[8px] text-[14px] opacity-40">
+					<div class="flex">
+						<Icon name="Checkmark" />
+						See in full 1,100+ hight quality guides on various topics.
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Mark any topic as learned / to learn / learning.
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Mark any link you find in LA as completed or to complete later.
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Add your own links and track progress on them.
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Publish your notes to your own personal wiki page.
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Sync all your notes with a mobile app (beta).
+					</div>
+					<div class="flex">
+						<Icon name="Checkmark" />
+						Ai interface to all your notes (beta).
+					</div>
+				</div>
+				<div class="font-bold text-[14px] col-gap-[16px]">
+					<div
+						class=" rounded-[10px] border border-white text-dark"
+						style="background: linear-gradient(rgb(255, 255, 255), rgb(210, 210, 210)); filter: drop-shadow(rgb(0, 0, 0) 0px 4px 6px);"
+					>
+						<div
+							class="w-full h-full p-[8px] px-[14px] flex-center"
+							style="background: linear-gradient(rgba(255, 255, 255, 0), rgba(35, 88, 224, 0.125));"
+						>
+							Join for $96/year
+						</div>
+					</div>
+					<div class="text-[14px] opacity-40 font-light w-full flex-center">Refund Policy</div>
+				</div>
+			</div>
+		</div>
+	)
 }
