4afebc270b4c75a0894f02564dc5711074d0c884
Nikita <nikita.voloboev@gmail.com>
2024-04-02 11:52:58 +0400

    .

--- website/src/routes/auth.tsx
+++ website/src/routes/auth.tsx
@@ -20,8 +20,6 @@ export default function Auth() {
 		if (import.meta.env.VITE_ENV !== "prod" && import.meta.env.VITE_ENV !== "staging") {
 			return
 		}
-		// VITE_CF_PAGES_BRANCH
-		// console.log(import.meta.env.CF_PAGES_BRANCH, "branch")
 		console.log(import.meta.env.VITE_CF_PAGES_BRANCH, "branch")
 		if (import.meta.env.VITE_ENV === "staging") {
 			// TODO: get valid token from hanko safely as a dev
