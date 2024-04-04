96f4e8108ad04df2e4557b525e49583705bc44c8
Nikita <nikita.voloboev@gmail.com>
2024-04-04 12:47:05 +0400

    .

--- website/components/DevDeployedBranch.tsx
+++ website/components/DevDeployedBranch.tsx
@@ -1,23 +1,33 @@
+import { Show, createSignal, onMount } from "solid-js"
 import { Motion } from "solid-motionone"
 
 export default function DevDeployedBranch() {
 	if (import.meta.env.VITE_ENV === "dev" && import.meta.env.VITE_CF_PAGES_BRANCH) {
+		const [isVisible, setIsVisible] = createSignal(true)
+		onMount(() => {
+			setTimeout(() => {
+				setIsVisible(false)
+			}, 4000)
+		})
+
 		return (
-			<Motion.div
-				animate={{
-					opacity: [0, 1],
-					transform: ["translate(20px, -20px) scale(0.9)", "translate(0, 0) scale(1)"],
-				}}
-				class="absolute top-5 right-5 bg-hoverDark p-2 px-4 rounded-[7px] text-white/50"
-			>
-				<a
-					href={`https://github.com/learn-anything/learn-anything.xyz/pull/${
-						import.meta.env.VITE_CF_PAGES_BRANCH
-					}`}
+			<Show when={isVisible()} fallback={<></>}>
+				<Motion.div
+					animate={{
+						opacity: [0, 1],
+						transform: ["translate(20px, -20px) scale(0.9)", "translate(0, 0) scale(1)"],
+					}}
+					class="absolute top-5 right-5 bg-hoverDark p-2 px-4 rounded-[7px] text-white/50"
 				>
-					Deployed branch: {import.meta.env.VITE_CF_PAGES_BRANCH}
-				</a>
-			</Motion.div>
+					<a
+						href={`https://github.com/learn-anything/learn-anything.xyz/pull/${
+							import.meta.env.VITE_CF_PAGES_BRANCH
+						}`}
+					>
+						Deployed branch: {import.meta.env.VITE_CF_PAGES_BRANCH}
+					</a>
+				</Motion.div>
+			</Show>
 		)
 	} else {
 		return <></>
