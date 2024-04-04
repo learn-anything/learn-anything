d5e8b898fdf5c12562e595876a65c1a9684cde90
Nikita <nikita.voloboev@gmail.com>
2024-04-04 13:17:10 +0400

    .

--- website/src/routes/auth.tsx
+++ website/src/routes/auth.tsx
@@ -119,6 +119,7 @@ export default function Auth() {
 				<div class="absolute bottom-5 left-[50%] translate-x-[-50%] text-[14px] opacity-20">
 					<div>Learn Anything</div>
 				</div>
+				{/* TODO: maybe move it to show in all pages in dev. (put it in app.tsx?) */}
 				<DevDeployedBranch />
 			</div>
 		</>
