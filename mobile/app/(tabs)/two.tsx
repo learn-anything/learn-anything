506617cebbd258122cbaeb510c2324e7e247a3c7
marshennikovaolga <marshennikova@gmail.com>
2024-04-04 19:40:06 +0400

    auth pin code functionality

--- mobile/app/(tabs)/two.tsx
+++ mobile/app/(tabs)/two.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React, {
+	useState,
+	useEffect,
+	useRef,
+	KeyboardEvent,
+	ChangeEvent,
+} from "react"
 import {
 	View,
 	StyleSheet,
@@ -26,25 +32,79 @@ const isValidEmail = (email: string): boolean => {
 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	return emailPattern.test(email.toLowerCase())
 }
-const useDebounce = (value: any, delay: number) => {
-	const [debouncedValue, setDebouncedValue] = useState(value)
 
-	useEffect(() => {
-		const handler = setTimeout(() => {
-			setDebouncedValue(value)
-		}, delay)
+// pass code
+type InputProps = {
+	length?: number
+	onComplete: (pin: string) => void
+}
 
-		return () => {
-			clearTimeout(handler)
+const OTPInput = ({ length = 6, onComplete }: InputProps) => {
+	const inputRef = useRef<Array<TextInput | null>>(Array(length).fill(null))
+	const [OTP, setOTP] = useState<Array<string>>(Array(length).fill(""))
+	const [currentInput, setCurrentInput] = useState(-1)
+
+	const handleTextChange = (input: string, index: number) => {
+		const newPin = [...OTP]
+		newPin[index] = input.replace(/[^0-9]/g, "")
+		setOTP(newPin)
+
+		if (input) {
+			if (index < length - 1) {
+				inputRef.current[index + 1]?.focus()
+			}
+		} else {
+			if (index > 0 && newPin.slice(0, index).some((digit) => digit !== "")) {
+				inputRef.current[index - 1]?.focus()
+			}
 		}
-	}, [value, delay])
+		if (newPin.every((digit) => digit !== "")) {
+			onComplete(newPin.join(""))
+		}
+	}
 
-	return debouncedValue
+	const handleKeyPress = ({
+		nativeEvent: { key },
+		index,
+	}: {
+		nativeEvent: { key: string }
+		index: number
+	}) => {
+		if (key === "Backspace" && index > 0 && OTP[index] === "") {
+			const newOTP = [...OTP]
+			newOTP[index - 1] = ""
+			setOTP(newOTP)
+			inputRef.current[index - 1]?.focus()
+		}
+	}
+
+	return (
+		<View style={styles.numberBoxContainer}>
+			{Array.from({ length }, (_, index) => (
+				<TextInput
+					key={index}
+					keyboardType="numeric"
+					maxLength={1}
+					onChangeText={(text) => handleTextChange(text, index)}
+					onKeyPress={(e) =>
+						handleKeyPress({ nativeEvent: e.nativeEvent, index })
+					}
+					onFocus={() => setCurrentInput(index)}
+					style={[
+						styles.numberBox,
+						index !== length - 1 ? { marginRight: 10 } : {},
+						currentInput === index ? styles.currentInputColor : {},
+					]}
+					ref={(el) => (inputRef.current[index] = el)}
+				/>
+			))}
+		</View>
+	)
 }
 
 export default function TabTwoScreen() {
 	const [email, setEmail] = useState("")
-	const debouncedEmail = useDebounce(email, 300)
+	const [isSubmitted, setIsSubmitted] = useState(false)
 
 	return (
 		<View style={styles.container}>
@@ -160,59 +220,87 @@ export default function TabTwoScreen() {
 					<Text style={styles.welcomeTitle}>Welcome</Text>
 				</View>
 
-				<View style={styles.inputContainer}>
-					<View
-						style={{
-							flexDirection: "row",
-							alignItems: "center",
-							justifyContent: "center",
-							height: 50,
-							marginTop: 30,
-							width: "95%",
-						}}
-					>
-						<TextInput
-							style={[
-								styles.input,
-								email.trim() !== "" ? styles.inputActive : {},
-								isValidEmail(email) ? styles.inputValid : {},
-							]}
-							placeholder="Enter email"
-							textAlign="center"
-							placeholderTextColor="rgba(255, 255, 255, 0.2)"
-							autoCapitalize="none"
-							onChangeText={(text) => setEmail(text.toLowerCase())}
-						/>
-						{isValidEmail(email) && (
-							<TouchableOpacity style={styles.continueButton}>
-								<Text style={styles.continueText}>Continue</Text>
-							</TouchableOpacity>
-						)}
-					</View>
+				{/* number box */}
 
-					<Text
-						style={{ color: "rgba(255, 255, 255, 0.3)", marginVertical: 15 }}
-					>
-						or
-					</Text>
-					<TouchableOpacity style={styles.passkeyButton}>
-						<Text style={styles.passkeyText}>Sign in with a passkey</Text>
-					</TouchableOpacity>
-				</View>
-				<Text style={styles.textWarning}>
-					By clicking on either button, you agree to the{" "}
-					<Text
-						style={{
-							textDecorationLine: "underline",
-							textDecorationStyle: "solid",
-							paddingBottom: 2, // wtf doesnt work
-							borderBottomWidth: 1,
-							lineHeight: 24,
-						}}
-					>
-						Terms of Service
-					</Text>
-				</Text>
+				{isSubmitted ? (
+					<>
+						<OTPInput
+							length={6}
+							onComplete={(pin) => {
+								console.log("Entered PIN:", pin)
+							}}
+						/>
+						<Text style={styles.textWarning}>
+							Enter the code that was sent to user.Email
+						</Text>
+						<TouchableOpacity style={styles.resendCodeButton}>
+							<Text style={styles.resendCodeText}>Resend code</Text>
+						</TouchableOpacity>
+						<Text style={styles.backText}>Back</Text>
+					</>
+				) : (
+					<>
+						<View style={styles.inputContainer}>
+							<View
+								style={{
+									flexDirection: "row",
+									alignItems: "center",
+									justifyContent: "center",
+									height: 50,
+									marginTop: 30,
+									width: "95%",
+								}}
+							>
+								<TextInput
+									style={[
+										styles.input,
+										email.trim() !== "" ? styles.inputActive : {},
+										isValidEmail(email) ? styles.inputValid : {},
+									]}
+									placeholder="Enter email"
+									textAlign="center"
+									placeholderTextColor="rgba(255, 255, 255, 0.2)"
+									autoCapitalize="none"
+									onChangeText={(text) => setEmail(text.toLowerCase())}
+								/>
+								{isValidEmail(email) && (
+									<TouchableOpacity
+										style={styles.continueButton}
+										onPress={() => setIsSubmitted(true)}
+									>
+										<Text style={styles.continueText}>Continue</Text>
+									</TouchableOpacity>
+								)}
+							</View>
+
+							<Text
+								style={{
+									color: "rgba(255, 255, 255, 0.3)",
+									marginVertical: 15,
+								}}
+							>
+								or
+							</Text>
+							<TouchableOpacity style={styles.passkeyButton}>
+								<Text style={styles.passkeyText}>Sign in with a passkey</Text>
+							</TouchableOpacity>
+						</View>
+						<Text style={styles.textWarning}>
+							By clicking on either button, you agree to the{" "}
+							<Text
+								style={{
+									textDecorationLine: "underline",
+									textDecorationStyle: "solid",
+									paddingBottom: 2, // wtf doesnt work
+									borderBottomWidth: 1,
+									lineHeight: 24,
+								}}
+							>
+								Terms of Service
+							</Text>
+						</Text>
+					</>
+				)}
 			</View>
 			<View style={styles.linkContainer}>
 				<Text style={{ color: "white", opacity: 0.3 }}>Learn Anything</Text>
@@ -362,5 +450,53 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		fontWeight: "500",
 		textAlign: "center",
+	},
+	numberBoxContainer: {
+		marginTop: 30,
+		marginBottom: 14,
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+	},
+	numberBox: {
+		width: 48,
+		height: 45,
+		borderRadius: 7,
+		borderColor: "rgba(255, 255, 255, 0.03)",
+		borderWidth: 1,
+		backgroundColor: "rgb(32 32 32)",
+		color: "white",
+		fontSize: 16,
+		fontWeight: "500",
+		textAlign: "center",
+	},
+	currentInputColor: {
+		backgroundColor: "#181a1f",
+	},
+	resendCodeButton: {
+		width: 122,
+		padding: 11,
+		borderWidth: 1,
+		backgroundColor: "#232323",
+		borderRadius: 7,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 1 },
+		shadowOpacity: 0.55,
+		shadowRadius: 1,
+	},
+	resendCodeText: {
+		color: "rgba(255, 255, 255, 1)",
+		opacity: 0.7,
+		fontSize: 16,
+		fontWeight: "500",
+		textAlign: "center",
+	},
+	backText: {
+		fontSize: 16,
+		fontWeight: "500",
+		textAlign: "center",
+		color: "white",
+		opacity: 0.5,
+		marginTop: 17,
 	},
 })
