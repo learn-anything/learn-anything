91ae9e505a29c81a4dcbb252548d20274ba0eac9
marshennikovaolga <marshennikova@gmail.com>
2024-04-05 16:09:17 +0400

    note description

--- mobile/app/(tabs)/note.tsx
+++ mobile/app/(tabs)/note.tsx
@@ -15,7 +15,9 @@ const { height } = Dimensions.get("window")
 
 export default function Note() {
 	const [text, setText] = useState("")
+	const [secondText, setSecondText] = useState("")
 	const inputRef = useRef<TextInput>(null)
+	const [isFocused, setIsFocused] = useState(false)
 
 	useEffect(() => {
 		inputRef.current?.focus()
@@ -27,14 +29,29 @@ export default function Note() {
 				<View style={styles.inputContainer}>
 					<TextInput
 						ref={inputRef}
-						style={styles.input}
+						style={[styles.input, !text && !isFocused ? styles.placeholderStyle : {}]}
 						multiline
-						placeholder="Enter your note..."
-						placeholderTextColor="rgba(255, 255, 255, 0.2)"
+						placeholder="Untitled"
+						placeholderTextColor="rgba(255, 255, 255, 0.3)"
 						value={text}
 						onChangeText={setText}
 					/>
-					<TouchableOpacity style={styles.clearButton} onPress={() => setText("")}>
+					<TextInput
+						ref={inputRef}
+						style={[styles.secondInput, !text && !isFocused ? styles.smallPlaceholderStyle : {}]}
+						multiline
+						placeholder="tap here to add a note..."
+						placeholderTextColor="rgba(255, 255, 255, 0.3)"
+						value={secondText}
+						onChangeText={setSecondText}
+					/>
+					<TouchableOpacity
+						style={styles.clearButton}
+						onPress={() => {
+							setText("")
+							setSecondText("")
+						}}
+					>
 						<Feather name="delete" size={24} color="gray" />
 					</TouchableOpacity>
 				</View>
@@ -68,16 +85,35 @@ const styles = StyleSheet.create({
 	inputContainer: {
 		position: "relative",
 	},
+	placeholderStyle: {
+		fontSize: 20,
+	},
+	smallPlaceholderStyle: {
+		fontSize: 15,
+	},
 	input: {
 		fontSize: 16,
 		fontWeight: "500",
 		color: "white",
-		padding: 10,
+		paddingTop: 25,
+		paddingHorizontal: 11,
 		borderRadius: 10,
-		backgroundColor: "#1F1F1F",
+		backgroundColor: "rgba(31, 31, 31, 0.2)",
+		// minHeight: height * 0.1,
+		maxHeight: height * 0.2,
+		lineHeight: 20,
+		paddingRight: width * 0.06,
+	},
+	secondInput: {
+		fontSize: 14,
+		fontWeight: "400",
+		color: "white",
+		paddingTop: 15,
+		paddingHorizontal: 11,
+		borderRadius: 10,
+		backgroundColor: "rgba(31, 31, 31, 0.2)",
 		minHeight: height * 0.1,
 		maxHeight: height * 0.9,
-		paddingTop: 15,
 		lineHeight: 20,
 		paddingRight: width * 0.06,
 	},
