c1e9aae68f9f1e7b68b4a2798ec753032b090477
Nikita <nikita.voloboev@gmail.com>
2024-04-08 15:34:12 +0400

    .

--- shared/edgedb.ts
+++ shared/edgedb.ts
@@ -1,9 +1,18 @@
-import e from "../dbschema/edgeql-js"
+import { client } from "../api/edgedb/client"
+import e from "../api/edgedb/dbschema/edgeql-js"
 
 export function foundUserByEmail(email: string) {
   return e.select(e.User, (user) => ({
     filter_single: e.op(user.email, "=", email)
   }))
+}
+
+// returns id of `Other` object
+// it should be enforced that there is only one `Other` object in db, query relies on it to work
+// TODO: https://discord.com/channels/841451783728529451/1226854877431599175
+export async function foundOtherObjectId() {
+  const res = await e.select(e.Other).run(client)
+  return res[0]!.id
 }
 
 // for queries that use above function to find user, this is what is expected on mutation queries
