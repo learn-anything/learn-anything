2f96f80a889d244e39c6d40e434a728ea6a9c7a3
marshennikovaolga <marshennikova@gmail.com>
2024-04-08 17:57:50 +0400

    animated suggestions added

--- mobile/app/(tabs)/search.tsx
+++ mobile/app/(tabs)/search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import {
 	SafeAreaView,
 	View,
@@ -9,31 +9,56 @@ import {
 	TextInput,
 	Keyboard,
 	TouchableWithoutFeedback,
+	ScrollView,
+	Animated,
 } from "react-native"
 import AntDesign from "@expo/vector-icons/AntDesign"
-import Svg, {
-	G,
-	Path,
-	Defs,
-	LinearGradient as SvgLinearGradient,
-	Stop,
-	ClipPath,
-	Rect,
-	RadialGradient,
-	Circle,
-} from "react-native-svg"
+import suggestions from "../constants/suggestions"
 
 const { width } = Dimensions.get("window")
 
 export default function Search() {
 	const [searchQuery, setSearchQuery] = useState("")
-
+	const [showSuggestions, setShowSuggestions] = useState(false)
+	const animations = useRef(new Animated.Value(0)).current
 	const handleSearch = () => {
 		console.log("search:", searchQuery)
 	}
 
+	const filteredSuggestions = suggestions.filter((suggestion: string) =>
+		suggestion.toLowerCase().startsWith(searchQuery.toLowerCase()),
+	)
+
+	const handleSuggestionClick = (suggestion: string) => {
+		setSearchQuery(suggestion)
+		setShowSuggestions(false)
+	}
+
+	const suggestionHeight = 50
+	const suggestionsMaxHeight = 200
+
+	// animate suggestions
+	const dynamicHeight = Math.min(
+		filteredSuggestions.length * suggestionHeight,
+		suggestionsMaxHeight,
+	)
+
+	useEffect(() => {
+		Animated.timing(animations, {
+			toValue: showSuggestions && searchQuery ? dynamicHeight : 0,
+			duration: 700,
+			useNativeDriver: false,
+		}).start()
+	}, [showSuggestions, searchQuery, filteredSuggestions.length])
+
 	return (
-		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss()
+				setShowSuggestions(false)
+			}}
+			accessible={false}
+		>
 			<SafeAreaView style={styles.container}>
 				<View style={styles.searchContainer}>
 					<TextInput
@@ -41,7 +66,11 @@ export default function Search() {
 						placeholder="search or enter topic name..."
 						placeholderTextColor="rgba(255, 255, 255, 0.5)"
 						value={searchQuery}
-						onChangeText={setSearchQuery}
+						onChangeText={(text) => {
+							setSearchQuery(text)
+							setShowSuggestions(true)
+						}}
+						onFocus={() => setShowSuggestions(true)}
 					/>
 					<TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
 						<AntDesign
@@ -51,6 +80,21 @@ export default function Search() {
 						/>
 					</TouchableOpacity>
 				</View>
+				<Animated.View
+					style={[styles.suggestionsContainer, { height: animations }]}
+				>
+					<ScrollView>
+						{filteredSuggestions.map((suggestion: string, index: number) => (
+							<TouchableOpacity
+								key={index}
+								style={styles.suggestionTopic}
+								onPress={() => handleSuggestionClick(suggestion)}
+							>
+								<Text style={styles.suggestionText}>{suggestion}</Text>
+							</TouchableOpacity>
+						))}
+					</ScrollView>
+				</Animated.View>
 			</SafeAreaView>
 		</TouchableWithoutFeedback>
 	)
@@ -67,6 +111,9 @@ const styles = StyleSheet.create({
 		width: width * 0.9,
 		flexDirection: "row",
 		alignItems: "center",
+		position: "absolute",
+		top: 250,
+		zIndex: 1,
 	},
 	searchInput: {
 		flex: 1,
@@ -88,5 +135,23 @@ const styles = StyleSheet.create({
 		shadowOpacity: 0.55,
 		shadowRadius: 1,
 		padding: 10,
+	},
+	suggestionsContainer: {
+		marginTop: 10,
+		width: width * 0.9,
+		height: 100,
+		position: "absolute",
+		top: 300,
+	},
+	suggestionTopic: {
+		padding: 10,
+		borderColor: "#333",
+		borderWidth: 1,
+		borderRadius: 7,
+		marginBottom: 2,
+	},
+	suggestionText: {
+		color: "rgba(255, 255, 255, 0.8)",
+		fontSize: 16,
 	},
 })
