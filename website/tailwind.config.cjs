cfdc32570fe4be993d79d318d217ebb8dbfb5652
Nikita <nikita.voloboev@gmail.com>
2024-04-09 19:37:55 +0400

    .

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 module.exports = {
-	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./components/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		colors: {
 			white: "#ffffff",
