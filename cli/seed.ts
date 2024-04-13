98e314494c1c2ed1b7fb9b8456c0b521a3f19f20
Nikita <nikita.voloboev@gmail.com>
2024-04-13 20:37:50 +0400

    .

---
+++ cli/seed.ts
@@ -0,0 +1,32 @@
+async function seed() {
+	const args = Bun.argv
+	const command = args[2]
+	try {
+		switch (command) {
+			case "webIndex":
+				await webIndex()
+				break
+			case "mobileIndex":
+				await mobileIndex()
+				break
+			case undefined:
+				console.log("No command provided")
+				break
+			default:
+				console.log("Unknown command")
+				break
+		}
+	} catch (err) {
+		console.error("Error occurred:", err)
+	}
+}
+
+async function webIndex() {
+	// clear first
+}
+
+async function mobileIndex() {
+	// clear first
+}
+
+await seed()
