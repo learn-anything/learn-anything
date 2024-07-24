287797350c25af280c1d88b680577f30f366c9b7
Aslam H <iupin5212@gmail.com>
2024-07-24 19:21:04 +0700

    refactor: remove dnd provider

--- web/lib/providers/dnd-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { DndProvider as BaseDndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
-
-export const DndProvider = ({ children }: { children: React.ReactNode }) => {
-  return <BaseDndProvider backend={HTML5Backend}>{children}</BaseDndProvider>
-}
