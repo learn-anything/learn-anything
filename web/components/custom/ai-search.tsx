42e7319851b8d6ea5c87e8e11419975a9746509f
marshennikovaolga <marshennikova@gmail.com>
2024-07-25 21:22:57 +0400

    .

--- web/components/custom/ai-search.tsx
+++ web/components/custom/ai-search.tsx
@@ -9,13 +9,13 @@ const AiSearch: React.FC<AiSearchProps> = ({ searchQuery }) => {
   return (
     <div className="mx-auto flex max-w-3xl flex-col items-center">
       <div className="w-full rounded-lg bg-inherit p-6 text-white">
-        <div className="mb-6 rounded-lg bg-blue-600 p-4">
-          <h2 className="text-xl font-semibold">
-            ✨ This is what I have found:
-          </h2>
+        <div className="mb-6 rounded-lg bg-blue-700 p-4">
+          <h2 className="text-lg font-medium">✨ This is what I have found:</h2>
         </div>
         <div className="rounded-xl bg-[#121212] p-4">
-          <h1 className="text-md mb-4 text-white opacity-50">{searchQuery}</h1>
+          <h1 className="text-md mb-4 border-b border-neutral-800 pb-2 font-semibold tracking-wider text-white opacity-50">
+            {searchQuery}
+          </h1>
           <p className="min-h-[100px] whitespace-pre-wrap">
             {/* AI content */}
           </p>
