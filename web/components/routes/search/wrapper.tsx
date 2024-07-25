452542c2421acd1b1cde8d1e8cb573d4f56551c1
marshennikovaolga <marshennikova@gmail.com>
2024-07-25 16:28:42 +0400

    .

--- web/components/routes/search/wrapper.tsx
+++ web/components/routes/search/wrapper.tsx
@@ -1,76 +1,8 @@
 "use client"
 import { useState } from "react"
 import { useAccount } from "@/lib/providers/jazz-provider"
-import { Icons } from "next/dist/lib/metadata/types/metadata-types"
-import { IoSearch, IoCloseOutline } from "react-icons/io5"
+import { IoSearch, IoCloseOutline, IoChevronForward } from "react-icons/io5"
 import { SearchHeader } from "./header"
-
-export const SearchWrapper = () => {
-  const account = useAccount()
-  const [searchText, setSearchText] = useState("")
-
-  const clearSearch = () => {
-    setSearchText("")
-  }
-
-  // todo: remove Y scroll
-  return (
-    <div className="flex h-full flex-auto flex-col overflow-hidden">
-      <SearchHeader />
-
-      <div className="box-border flex h-screen w-screen justify-center overflow-hidden">
-        <div className="w-[55%] max-w-5xl">
-          <div className="relative mb-2 mt-5 flex w-full flex-row items-center transition-colors duration-300 hover:text-white/60">
-            <IoSearch className="absolute left-2 text-white/30" size={20} />
-            <input
-              type="text"
-              autoFocus
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-              className="w-full rounded-[10px] bg-[#16181d] p-10 py-3 font-light tracking-wider opacity-70 outline-none placeholder:font-light placeholder:text-white/30"
-              placeholder="Search..."
-            />
-            {searchText && (
-              <IoCloseOutline
-                className="absolute right-2 cursor-pointer opacity-30"
-                size={20}
-                onClick={clearSearch}
-              />
-            )}
-          </div>
-          <div className="mx-auto my-5 justify-center space-y-1">
-            <p className="pb-3 pl-2 text-base font-light opacity-50">
-              Topics <span className="opacity-70">1</span>
-            </p>
-            <ProfileTopics topic="Figma" />
-          </div>
-
-          <div className="mx-auto my-5 justify-center space-y-1">
-            <p className="pb-3 pl-2 text-base font-light opacity-50">
-              Links <span className="text-white/70">3</span>
-            </p>
-            <ProfileLinks
-              linklabel="Figma"
-              link="https://figma.com"
-              topic="Figma"
-            />
-            <ProfileLinks
-              linklabel="Figma"
-              link="https://figma.com"
-              topic="Figma"
-            />
-            <ProfileLinks
-              linklabel="Figma"
-              link="https://figma.com"
-              topic="Figma"
-            />
-          </div>
-        </div>
-        {/* </div> */}
-      </div>
-    </div>
-  )
-}
 
 interface ProfileTopicsProps {
   topic: string
@@ -80,7 +12,7 @@ const ProfileTopics: React.FC<ProfileTopicsProps> = ({ topic }) => {
   return (
     <div className="flex cursor-pointer flex-row items-center justify-between rounded-lg bg-[#121212] p-3 text-white">
       <p>{topic}</p>
-      {/* <icons.VectorArrowRight /> */}
+      <IoChevronForward className="text-white/50" size={20} />
     </div>
   )
 }
@@ -109,6 +41,71 @@ const ProfileLinks: React.FC<ProfileLinksProps> = ({
       </div>
       <div className="cursor-default rounded-lg bg-[#1a1a1a] p-2 text-white/50">
         {topic}
+      </div>
+    </div>
+  )
+}
+
+export const SearchWrapper = () => {
+  const account = useAccount()
+  const [searchText, setSearchText] = useState("")
+
+  const clearSearch = () => {
+    setSearchText("")
+  }
+
+  return (
+    <div className="flex h-full flex-auto flex-col overflow-hidden">
+      <SearchHeader />
+
+      <div className="flex h-full w-full justify-center overflow-hidden">
+        <div className="w-full max-w-3xl px-4 sm:px-6 lg:px-8">
+          <div className="relative mb-2 mt-5 flex w-full flex-row items-center transition-colors duration-300 hover:text-white/60">
+            <IoSearch className="absolute left-3 text-white/30" size={20} />
+            <input
+              type="text"
+              autoFocus
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              className="w-full rounded-[10px] bg-[#16181d] p-10 py-3 pl-10 pr-3 font-light tracking-wider opacity-70 outline-none placeholder:font-light placeholder:text-white/30"
+              placeholder="Search..."
+            />
+            {searchText && (
+              <IoCloseOutline
+                className="absolute right-3 cursor-pointer opacity-30"
+                size={20}
+                onClick={clearSearch}
+              />
+            )}
+          </div>
+          <div className="my-5 space-y-1">
+            <p className="pb-3 pl-2 text-base font-light opacity-50">
+              Topics <span className="opacity-70">1</span>
+            </p>
+            <ProfileTopics topic="Figma" />
+          </div>
+
+          <div className="my-5 space-y-1">
+            <p className="pb-3 pl-2 text-base font-light opacity-50">
+              Links <span className="text-white/70">3</span>
+            </p>
+            <ProfileLinks
+              linklabel="Figma"
+              link="https://figma.com"
+              topic="Figma"
+            />
+            <ProfileLinks
+              linklabel="Figma"
+              link="https://figma.com"
+              topic="Figma"
+            />
+            <ProfileLinks
+              linklabel="Figma"
+              link="https://figma.com"
+              topic="Figma"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
