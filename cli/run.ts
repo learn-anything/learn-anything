72641bff140e48b25dc7ac7fa6c480ba6984dfc3
Nikita <github@nikiv.dev>
2024-07-26 16:00:23 +0400

    .

--- cli/run.ts
+++ cli/run.ts
@@ -1,5 +1,9 @@
+import { getEnvOrThrow } from "@/lib/utils"
+
 async function run() {
 	try {
+		const OPENAI_API_KEY = getEnvOrThrow("OPENAI_API_KEY")
+		console.log(OPENAI_API_KEY)
 	} catch (err) {
 		console.log(err, "err")
 	}
