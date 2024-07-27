5147384ca1e4163e94a1181d72732af7ccb708ed
marshennikovaolga <marshennikova@gmail.com>
2024-07-27 13:17:34 +0400

    .

--- web/components/routes/globalTopic/globalTopic.tsx
+++ web/components/routes/globalTopic/globalTopic.tsx
@@ -1,7 +1,27 @@
 "use client"
 import React, { useState } from "react"
 import { ContentHeader } from "@/components/custom/content-header"
+import { PiLinkSimple } from "react-icons/pi"
 // import { GraduationCap, Check } from "lucide-react"
+
+interface LinkProps {
+  title: string
+  url: string
+}
+
+const LinkItem: React.FC<LinkProps> = ({ title, url }) => (
+  <div className="mb-1 flex flex-row items-center justify-between rounded-xl bg-[#121212] px-2 py-4 hover:cursor-pointer">
+    <div className="flex items-center space-x-4">
+      <p>{title}</p>
+      <span className="text-md flex flex-row items-center space-x-1 font-medium tracking-wide text-white/20 hover:opacity-50">
+        <PiLinkSimple size={20} className="text-white/20" />
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {new URL(url).hostname}
+        </a>
+      </span>
+    </div>
+  </div>
+)
 
 const Button = ({
   children,
@@ -36,36 +56,38 @@ export default function GlobalTopic({ topic }: { topic: string }) {
 
   return (
     <div className="flex h-full flex-auto flex-col overflow-hidden">
-      <ContentHeader title={decodedTopic}>
-        <div className="flex items-center">
-          <div className="flex rounded-lg bg-neutral-800 bg-opacity-60">
-            <button
-              onClick={() => setActiveTab("Guide")}
-              className={`px-4 py-2 text-[16px] font-semibold transition-colors ${
-                activeTab === "Guide"
-                  ? "rounded-lg bg-neutral-800 shadow-inner shadow-neutral-700/70"
-                  : "text-white/70"
-              }`}
-            >
-              Guide
-            </button>
-            <button
-              onClick={() => setActiveTab("All links")}
-              className={`px-4 py-2 text-[16px] font-semibold transition-colors ${
-                activeTab === "All links"
-                  ? "rounded-lg bg-neutral-800 shadow-inner shadow-neutral-700/70"
-                  : "text-white/70"
-              }`}
-            >
-              All links
-            </button>
+      <ContentHeader>
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">{decodedTopic}</h1>
+          <div className="flex items-center space-x-4">
+            <div className="flex rounded-lg bg-neutral-800 bg-opacity-60">
+              <button
+                onClick={() => setActiveTab("Guide")}
+                className={`px-4 py-2 text-[16px] font-semibold transition-colors ${
+                  activeTab === "Guide"
+                    ? "rounded-lg bg-neutral-800 shadow-inner shadow-neutral-700/70"
+                    : "text-white/70"
+                }`}
+              >
+                Guide
+              </button>
+              <button
+                onClick={() => setActiveTab("All links")}
+                className={`px-4 py-2 text-[16px] font-semibold transition-colors ${
+                  activeTab === "All links"
+                    ? "rounded-lg bg-neutral-800 shadow-inner shadow-neutral-700/70"
+                    : "text-white/70"
+                }`}
+              >
+                All links
+              </button>
+            </div>
           </div>
         </div>
-
         <div className="relative">
           <Button
             onClick={() => setShowOptions(!showOptions)}
-            className="rounded-[7px] bg-neutral-800 px-4 py-2 text-[17px] font-semibold text-white/70 shadow-inner shadow-neutral-700/50 transition-colors hover:bg-neutral-700"
+            className="w-[150px] rounded-[7px] bg-neutral-800 px-4 py-2 text-[17px] font-semibold text-white/70 shadow-inner shadow-neutral-700/50 transition-colors hover:bg-neutral-700"
           >
             {selectedOption || "Add to my profile"}
           </Button>
@@ -84,6 +106,15 @@ export default function GlobalTopic({ topic }: { topic: string }) {
           )}
         </div>
       </ContentHeader>
+      <div className="px-5 py-3">
+        <h2 className="mb-3 text-white/60">Intro</h2>
+
+        <LinkItem title={"JavaScript"} url={"https://justjavascript.com"} />
+      </div>
+      <div className="px-5 py-3">
+        <h2 className="mb-3 text-opacity-60">Other</h2>
+        <LinkItem title={"JavaScript"} url={"https://justjavascript.com"} />
+      </div>
       <div className="flex-1 overflow-auto p-4"></div>
     </div>
   )
