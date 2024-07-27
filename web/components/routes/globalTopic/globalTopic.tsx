cbd76b57f7d6a6b1bd7be7856a3104cb8d067c1e
marshennikovaolga <marshennikova@gmail.com>
2024-07-27 16:40:36 +0400

    learning status

--- web/components/routes/globalTopic/globalTopic.tsx
+++ web/components/routes/globalTopic/globalTopic.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react"
 import { ContentHeader } from "@/components/custom/content-header"
 import { PiLinkSimple } from "react-icons/pi"
-// import { GraduationCap, Check } from "lucide-react"
+import { Bookmark, GraduationCap, Check } from "lucide-react"
 
 interface LinkProps {
   title: string
@@ -28,22 +28,32 @@ const LinkItem: React.FC<LinkProps> = ({ title, url }) => (
     </div>
   </div>
 )
-
-const Button = ({
-  children,
-  onClick,
-  className = ""
-}: {
+interface ButtonProps {
   children: React.ReactNode
   onClick: () => void
   className?: string
+  color?: string
+  icon?: React.ReactNode
+  fullWidth?: boolean
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  className = "",
+  color = "",
+  icon,
+  fullWidth = false
 }) => {
   return (
     <button
-      className={`flex items-center justify-between rounded px-3 py-1 text-sm font-medium ${className}`}
+      className={`flex items-center justify-start rounded px-3 py-1 text-sm font-medium ${
+        fullWidth ? "w-full" : ""
+      } ${className} ${color}`}
       onClick={onClick}
     >
-      {children}
+      {icon && <span className="mr-2 flex items-center">{icon}</span>}
+      <span>{children}</span>
     </button>
   )
 }
@@ -54,6 +64,25 @@ export default function GlobalTopic({ topic }: { topic: string }) {
   const [activeTab, setActiveTab] = useState("Guide")
 
   const decodedTopic = decodeURIComponent(topic)
+
+  const learningOptions = [
+    { text: "To Learn", icon: <Bookmark size={18} /> },
+    { text: "Learning", icon: <GraduationCap size={18} /> },
+    { text: "Learned", icon: <Check size={18} /> }
+  ]
+
+  const learningStatusColor = (option: string) => {
+    switch (option) {
+      case "To Learn":
+        return "text-white/70"
+      case "Learning":
+        return "text-[#D29752]"
+      case "Learned":
+        return "text-[#708F51]"
+      default:
+        return ""
+    }
+  }
 
   const selectedStatus = (option: string) => {
     setSelectedOption(option)
@@ -90,22 +119,31 @@ export default function GlobalTopic({ topic }: { topic: string }) {
             </div>
           </div>
         </div>
+
         <div className="relative">
           <Button
             onClick={() => setShowOptions(!showOptions)}
-            className="w-[150px] rounded-[7px] bg-neutral-800 px-4 py-2 text-[17px] font-semibold text-white/70 shadow-inner shadow-neutral-700/50 transition-colors hover:bg-neutral-700"
+            className="w-[150px] whitespace-nowrap rounded-[7px] bg-neutral-800 px-4 py-2 text-[17px] font-semibold text-white/70 shadow-inner shadow-neutral-700/50 transition-colors hover:bg-neutral-700"
+            color={learningStatusColor(selectedOption || "")}
+            icon={
+              selectedOption &&
+              learningOptions.find((opt) => opt.text === selectedOption)?.icon
+            }
           >
             {selectedOption || "Add to my profile"}
           </Button>
           {showOptions && (
             <div className="absolute left-1/2 mt-1 w-40 -translate-x-1/2 rounded-lg bg-neutral-800 shadow-lg">
-              {["To Learn", "Learning", "Learned"].map((option) => (
+              {learningOptions.map((option) => (
                 <Button
-                  key={option}
-                  onClick={() => selectedStatus(option)}
-                  className="w-full text-left text-[17px] font-semibold hover:bg-neutral-700"
+                  key={option.text}
+                  onClick={() => selectedStatus(option.text)}
+                  className="space-x-1 px-2 py-2 text-left text-[14px] font-semibold hover:bg-neutral-700"
+                  color={learningStatusColor(option.text)}
+                  icon={option.icon}
+                  fullWidth
                 >
-                  {option}
+                  {option.text}
                 </Button>
               ))}
             </div>
