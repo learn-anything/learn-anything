01d6043ea6e511ef36548a5969a165961e3286d1
Aslam H <iupin5212@gmail.com>
2024-07-29 23:05:39 +0700

    refactor: remove learn path

--- web/app/(pages)/learn/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LearnDetailWrapper } from "./_components/wrapper"
-
-export default function LearnDetailPage({
-  params
-}: {
-  params: { slug: string }
-}) {
-  return <LearnDetailWrapper slug={params.slug} />
-}
