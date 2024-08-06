9b0d02988fffca5e015542ceac6eb24a638e39a3
Aslam H <iupin5212@gmail.com>
2024-08-06 18:22:14 +0700

    fix: remove 'replace with learning status'

--- web/components/ui/checkbox.tsx
+++ web/components/ui/checkbox.tsx
@@ -21,7 +21,6 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <p>replace with learning status</p>
       <CheckIcon className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
