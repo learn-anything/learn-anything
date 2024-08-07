454332d674154fd520398f448d3ab63919b35cd5
Aslam H <iupin5212@gmail.com>
2024-08-07 11:41:20 +0700

    feat: handling data

--- web/components/la-editor/la-editor.tsx
+++ web/components/la-editor/la-editor.tsx
@@ -2,39 +2,125 @@
 
 import * as React from "react"
 import { EditorContent, useEditor } from "@tiptap/react"
+import { Editor } from "@tiptap/core"
+import { Content } from "@tiptap/core"
+import { useThrottleFn } from "react-use"
 import { BubbleMenu } from "./components/bubble-menu"
 import { createExtensions } from "./extensions"
 import "./styles/index.css"
 import { cn } from "@/lib/utils"
-export interface LAEditorProps extends React.HTMLProps<HTMLDivElement> {
+import { getOutput } from "./lib/utils"
+
+export interface LAEditorProps
+  extends Omit<React.HTMLProps<HTMLDivElement>, "value"> {
+  initialContent?: any
+  output?: "html" | "json" | "text"
   placeholder?: string
   editorClassName?: string
+  onUpdate?: (content: Content) => void
+  onBlur?: (Content: Content) => void
+  onNewBlock?: (content: Content) => void
+  value?: Content
+  throttleDelay?: number
 }
 
-export const LAEditor = React.forwardRef<HTMLDivElement, LAEditorProps>(
-  ({ placeholder, editorClassName, className, ...props }, ref) => {
-    const editor = useEditor(
-      {
-        autofocus: false,
-        extensions: createExtensions({ placeholder }),
-        editorProps: {
-          attributes: {
-            autocomplete: "off",
-            autocorrect: "off",
-            autocapitalize: "off",
-            class: editorClassName || ""
-          }
-        },
-        onUpdate: (props) => {
-          console.log(
-            "onUpdate",
-            props.editor.getHTML(),
-            props.editor.getJSON()
-          )
-        },
-        content: ``
+export interface LAEditorRef {
+  focus: () => void
+}
+
+interface CustomEditor extends Editor {
+  previousBlockCount?: number
+}
+
+export const LAEditor = React.forwardRef<LAEditorRef, LAEditorProps>(
+  (
+    {
+      initialContent,
+      value,
+      placeholder,
+      output = "html",
+      editorClassName,
+      className,
+      onUpdate,
+      onBlur,
+      onNewBlock,
+      throttleDelay = 1000,
+      ...props
+    },
+    ref
+  ) => {
+    const [content, setContent] = React.useState<Content | undefined>(value)
+    const throttledContent = useThrottleFn(
+      (defaultContent) => defaultContent,
+      throttleDelay,
+      [content]
+    )
+    const [lastThrottledContent, setLastThrottledContent] =
+      React.useState(throttledContent)
+
+    React.useEffect(() => {
+      if (lastThrottledContent !== throttledContent) {
+        setLastThrottledContent(throttledContent)
+        onUpdate?.(throttledContent!)
+      }
+    })
+
+    const editor = useEditor({
+      autofocus: false,
+      extensions: createExtensions({ placeholder }),
+      editorProps: {
+        attributes: {
+          autocomplete: "off",
+          autocorrect: "off",
+          autocapitalize: "off",
+          class: editorClassName || ""
+        }
       },
-      []
+      onCreate: ({ editor }) => {
+        if (editor.isEmpty && value) {
+          editor.commands.setContent(value)
+        }
+      },
+      onUpdate: ({ editor }) => {
+        setContent(getOutput(editor, output))
+
+        const customEditor = editor as CustomEditor
+        const json = customEditor.getJSON()
+
+        if (json.content && Array.isArray(json.content)) {
+          const currentBlockCount = json.content.length
+
+          if (
+            typeof customEditor.previousBlockCount === "number" &&
+            currentBlockCount > customEditor.previousBlockCount
+          ) {
+            onNewBlock?.(getOutput(editor, output))
+          }
+
+          customEditor.previousBlockCount = currentBlockCount
+        }
+      },
+      onBlur: ({ editor }) => {
+        onBlur?.(getOutput(editor, output))
+      }
+    })
+
+    React.useEffect(() => {
+      if (editor && initialContent) {
+        editor.commands.setContent(initialContent)
+      }
+    }, [initialContent])
+
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        focus: () => {
+          if (editor) {
+            editor.commands.focus()
+          }
+        }
+      }),
+      [editor]
     )
 
     if (!editor) {
@@ -48,7 +134,6 @@ export const LAEditor = React.forwardRef<HTMLDivElement, LAEditorProps>(
           className
         )}
         {...props}
-        ref={ref}
       >
         <EditorContent editor={editor} />
         <BubbleMenu editor={editor} />
--- web/components/la-editor/lib/utils/index.ts
+++ web/components/la-editor/lib/utils/index.ts
@@ -1,4 +1,14 @@
-export * from './keyboard'
-export * from './platform'
-export * from './isCustomNodeSelected'
-export * from './isTextSelected'
+import { Editor } from "@tiptap/core"
+import { LAEditorProps } from "../../la-editor"
+
+export function getOutput(editor: Editor, output: LAEditorProps["output"]) {
+  if (output === "html") return editor.getHTML()
+  if (output === "json") return editor.getJSON()
+  if (output === "text") return editor.getText()
+  return ""
+}
+
+export * from "./keyboard"
+export * from "./platform"
+export * from "./isCustomNodeSelected"
+export * from "./isTextSelected"
