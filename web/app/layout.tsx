66b692ee76c2116b3a99d19508c5d45abbdf9d8e
Aslam H <iupin5212@gmail.com>
2024-08-11 05:40:06 +0700

    chore: add suppressHydrationWarning

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en" className="h-full w-full">
+		<html lang="en" className="h-full w-full" suppressHydrationWarning>
 			<body className={cn("h-full w-full font-sans antialiased", inter.variable)}>
 				<JazzProvider>
 					<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
