28b9f25dc509e04303bec50828f65f71833b266a
Nikita <github@nikiv.dev>
2024-08-12 19:48:16 +0300

    .

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,7 @@ import ForceGraph from "@/components/routes/force-graph"
 
 export default function HomePage() {
 	// if authenticated, show the <Home (<Links)
-	console.log("running...")
+
 	return (
 		<div className="flex min-h-full items-center justify-center">
 			<ForceGraph />
