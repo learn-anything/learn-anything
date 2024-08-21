62e56e7623d7c678d9914506278f6ae812aa5a94
Nikita <github@nikiv.dev>
2024-08-21 11:44:50 +0300

    .

--- cli/seed.ts
+++ cli/seed.ts
@@ -44,25 +44,42 @@ async function seed() {
 	}
 }
 
-// sets up jazz global group and writes it to .env
+// sets up jazz global/admin group and writes it to .env
 async function setup() {
 	console.log("..")
 	const { worker } = await startWorker({
 		accountID: "co_zhvp7ryXJzDvQagX61F6RCZFJB9",
 		accountSecret: JAZZ_WORKER_SECRET
 	})
+<<<<<<< Updated upstream
 	console.log("creating")
 	const user = (await await LaAccount.createAs(worker, {
 		creationProps: { name: "nikiv" }
 	}))!
 	console.log("here?")
+||||||| Stash base
+	const user = (await await LaAccount.createAs(worker, {
+		creationProps: { name: "nikiv" }
+	}))!
+	console.log("here?")
+=======
+	//const user = (await await LaAccount.createAs(worker, {
+	//	creationProps: { name: "nikiv" }
+	//}))!
+	await new Promise(resolve => setTimeout(resolve, 1000))
+>>>>>>> Stashed changes
 	const publicGlobalGroup = PublicGlobalGroup.create({ owner: worker })
 	publicGlobalGroup.root = PublicGlobalGroupRoot.create({}, { owner: publicGlobalGroup })
 	publicGlobalGroup.addMember("everyone", "reader")
 	await appendFile("./.env", `\nJAZZ_PUBLIC_GLOBAL_GROUP=${JSON.stringify(publicGlobalGroup.id)}`)
 	const adminGlobalGroup = Group.create({ owner: worker })
-	adminGlobalGroup.addMember(user, "admin")
+	// adminGlobalGroup.addMember(user, "admin")
+	console.log("created admin group", adminGlobalGroup.id)
+	// console.log(user.id)
 	await appendFile("./.env", `\nJAZZ_ADMIN_GLOBAL_GROUP=${JSON.stringify(adminGlobalGroup.id)}`)
+
+	// make sure newly created groups have time to be synced
+	await new Promise(resolve => setTimeout(resolve, 1000))
 }
 
 async function prodSeed() {
@@ -125,66 +142,65 @@ async function prodSeed() {
 	} catch (error) {
 		console.error("Error reading directory:", error)
 	}
-
-	return
+}
 
 	// const folderPath = path.join(__dirname, "..", "private")
-	const files = await fs.readdir(folderPath)
-	for (const file of files) {
-		if (file === ".git") continue
-		const filePath = path.join(folderPath, file)
-		const stats = await fs.stat(filePath)
+	// const files = await fs.readdir(folderPath)
+	// for (const file of files) {
+	// 	if (file === ".git") continue
+	// 	const filePath = path.join(folderPath, file)
+	// 	const stats = await fs.stat(filePath)
 
-		if (stats.isDirectory()) {
-			// if datare folder go inside it and read files
-			if (file === "data") {
-				console.logturn
-				const dataFiles = await fs.readdir(filePath)
-				for (const subFile of dataFiles) {
-					const dataFilePath = path.join(filePath, subFile)
-					const content = await fs.readFile(dataFilePath, "utf-8")
-					const data = JSON.parse(content)
-					console.log(data, "data")
-				}
-			}
+	// 	if (stats.isDirectory()) {
+	// 		// if datare folder go inside it and read files
+	// 		if (file === "data") {
+	// 			console.logturn
+	// 			const dataFiles = await fs.readdir(filePath)
+	// 			for (const subFile of dataFiles) {
+	// 				const dataFilePath = path.join(filePath, subFile)
+	// 				const content = await fs.readFile(dataFilePath, "utf-8")
+	// 				const data = JSON.parse(content)
+	// 				console.log(data, "data")
+	// 			}
+	// 		}
 
-			if (file === "lastprod") {
-				console.log("lastprod folder")
-				const edgedbFiles = await fs.readdir(filePath)
-				for (const subFile of edgedbFiles) {
-					console.log(subFile, "sub file")
-					const edgedbFilePath = path.join(filePath, subFile)
-					console.log(edgedbFilePath, "file path")
-				}
-			}
-		} else if (stats.isFile()) {
-			if (file === "force-graph-connections.json") {
-				const content = await fs.readFile(filePath, "utf-8")
-				const topics = JSON.parse(content) as Array<{ name: string; prettyName: string; connections: string[] }>
+	// 		if (file === "lastprod") {
+	// 			console.log("lastprod folder")
+	// 			const edgedbFiles = await fs.readdir(filePath)
+	// 			for (const subFile of edgedbFiles) {
+	// 				console.log(subFile, "sub file")
+	// 				const edgedbFilePath = path.join(filePath, subFile)
+	// 				console.log(edgedbFilePath, "file path")
+	// 			}
+	// 		}
+	// 	} else if (stats.isFile()) {
+	// 		if (file === "force-graph-connections.json") {
+	// 			const content = await fs.readFile(filePath, "utf-8")
+	// 			const topics = JSON.parse(content) as Array<{ name: string; prettyName: string; connections: string[] }>
 
-				const createdTopics: { [name: string]: { node: GlobalTopic; connections: string[] } } = Object.fromEntries(
-					topics.map(topic => {
-						const node = GlobalTopic.create(
-							{
-								name: topic.name,
-								prettyName: topic.prettyName,
-								connections: ListOfGlobalTopics.create([], { owner: globalGroup })
-							},
-							{ owner: globalGroup }
-						)
+	// 			const createdTopics: { [name: string]: { node: GlobalTopic; connections: string[] } } = Object.fromEntries(
+	// 				topics.map(topic => {
+	// 					const node = GlobalTopic.create(
+	// 						{
+	// 							name: topic.name,
+	// 							prettyName: topic.prettyName,
+	// 							connections: ListOfGlobalTopics.create([], { owner: globalGroup })
+	// 						},
+	// 						{ owner: globalGroup }
+	// 					)
 
-						const connections = topic.connections
+	// 					const connections = topic.connections
 
-						return [topic.name, { node, connections }]
-					})
-				)
+	// 					return [topic.name, { node, connections }]
+	// 				})
+	// 			)
 
-				for (const [topicName, { node, connections }] of Object.entries(createdTopics)) {
-					for (const connection of connections) {
-						const connectionNode = createdTopics[connection].node
-						node.connections!.push(connectionNode)
-					}
-				}
+	// 			for (const [topicName, { node, connections }] of Object.entries(createdTopics)) {
+	// 				for (const connection of connections) {
+	// 					const connectionNode = createdTopics[connection].node
+	// 					node.connections!.push(connectionNode)
+	// 				}
+	// 			}
 				// const graph = GlobalTopicGraph.create(
 				// 	Object.values(createdTopics).map(({ node }) => node),
 				// 	{ owner: globalGroup }
@@ -192,18 +208,18 @@ async function prodSeed() {
 				// await writeToOutput(graph, "graph.json")
 				// globalGroup.root.topics = graph
 				// await new Promise(resolve => setTimeout(resolve, 1000))
-			}
-			if (file === "topics.json") {
-				const content = await fs.readFile(filePath, "utf-8")
-				const topics = JSON.parse(content) as Array<{ name: string; prettyName: string }>
+			// }
+			// if (file === "topics.json") {
+			// 	const content = await fs.readFile(filePath, "utf-8")
+			// 	const topics = JSON.parse(content) as Array<{ name: string; prettyName: string }>
 
-				topics.forEach(topic => {
-					GlobalTopic.create({ name: topic.name, prettyName: topic.prettyName }, { owner: globalGroup })
-				})
-			}
-		}
-	}
-}
+			// 	topics.forEach(topic => {
+			// 		GlobalTopic.create({ name: topic.name, prettyName: topic.prettyName }, { owner: globalGroup })
+			// 	})
+			// }
+		// }
+// 	}
+// }
 
 async function test() {
 	const folderPath = path.join(__dirname, "..", "private", "data", "edgedb", "topics")
@@ -221,11 +237,11 @@ async function fullProdRebuild() {
 	await prodSeed()
 }
 
-async function writeToOutput(content: any, file: string) {
-	const outputDir = path.join(__dirname, "..", "output")
-	await fs.mkdir(outputDir, { recursive: true })
-	const outputPath = path.join(outputDir, file)
-	await fs.writeFile(outputPath, JSON.stringify(content, null, 2))
-}
+// async function writeToOutput(content: any, file: string) {
+// 	const outputDir = path.join(__dirname, "..", "output")
+// 	await fs.mkdir(outputDir, { recursive: true })
+// 	const outputPath = path.join(outputDir, file)
+// 	await fs.writeFile(outputPath, JSON.stringify(content, null, 2))
+// }
 
 await seed()
