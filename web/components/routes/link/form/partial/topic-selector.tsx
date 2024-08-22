f858964130a3952d5f7663be2da462bea86ccc8b
Aslam H <iupin5212@gmail.com>
2024-08-22 21:35:47 +0700

    chore: implement topic from public global group

--- web/components/routes/link/form/partial/topic-selector.tsx
+++ web/components/routes/link/form/partial/topic-selector.tsx
@@ -3,19 +3,27 @@ import { Command, CommandInput, CommandList, CommandItem, CommandGroup } from "@
 import { FormField, FormItem, FormLabel, FormControl } from "@/components/ui/form"
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { CheckIcon, ChevronDownIcon } from "lucide-react"
+import { CheckIcon } from "lucide-react"
 import { useFormContext } from "react-hook-form"
 import { cn } from "@/lib/utils"
 import { useAtom } from "jotai"
 import { linkTopicSelectorAtom } from "@/store/link"
 import { LinkFormValues } from "../schema"
-
-const TOPICS = [
-	{ id: "1", name: "Work" },
-	{ id: "2", name: "Personal" }
-]
+import { useCoState } from "@/lib/providers/jazz-provider"
+import { PublicGlobalGroup } from "@/lib/schema/master/public-group"
+import { ID } from "jazz-tools"
+import { LaIcon } from "@/components/custom/la-icon"
 
 export const TopicSelector: React.FC = () => {
+	const globalGroup = useCoState(
+		PublicGlobalGroup,
+		process.env.NEXT_PUBLIC_JAZZ_GLOBAL_GROUP as ID<PublicGlobalGroup>,
+		{
+			root: {
+				topics: []
+			}
+		}
+	)
 	const [istopicSelectorOpen, setIstopicSelectorOpen] = useAtom(linkTopicSelectorAtom)
 	const form = useFormContext<LinkFormValues>()
 
@@ -31,9 +39,11 @@ export const TopicSelector: React.FC = () => {
 							<FormControl>
 								<Button size="sm" type="button" role="combobox" variant="secondary" className="gap-x-2 text-sm">
 									<span className="truncate">
-										{field.value ? TOPICS.find(topic => topic.name === field.value)?.name : "Topic"}
+										{field.value
+											? globalGroup?.root.topics.find(topic => topic?.id && topic.name === field.value)?.name
+											: "Topic"}
 									</span>
-									<ChevronDownIcon size={16} />
+									<LaIcon name="ChevronDown" />
 								</Button>
 							</FormControl>
 						</PopoverTrigger>
@@ -48,29 +58,32 @@ export const TopicSelector: React.FC = () => {
 								<CommandList>
 									<ScrollArea>
 										<CommandGroup>
-											{TOPICS.map(topic => (
-												<CommandItem
-													key={topic.id}
-													value={topic.name}
-													onClick={e => {
-														e.preventDefault()
-														e.stopPropagation()
-													}}
-													onSelect={value => {
-														field.onChange(value)
-														setIstopicSelectorOpen(false)
-													}}
-												>
-													{topic.name}
-													<CheckIcon
-														size={16}
-														className={cn(
-															"absolute right-3",
-															topic.name === field.value ? "text-primary" : "text-transparent"
-														)}
-													/>
-												</CommandItem>
-											))}
+											{globalGroup?.root.topics.map(
+												topic =>
+													topic?.id && (
+														<CommandItem
+															key={topic.id}
+															value={topic.name}
+															onClick={e => {
+																e.preventDefault()
+																e.stopPropagation()
+															}}
+															onSelect={value => {
+																field.onChange(value)
+																setIstopicSelectorOpen(false)
+															}}
+														>
+															{topic.prettyName}
+															<CheckIcon
+																size={16}
+																className={cn(
+																	"absolute right-3",
+																	topic.name === field.value ? "text-primary" : "text-transparent"
+																)}
+															/>
+														</CommandItem>
+													)
+											)}
 										</CommandGroup>
 									</ScrollArea>
 								</CommandList>
