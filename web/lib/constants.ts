8b42ce2be1df5cf97602972abb542470be371886
Aslam H <iupin5212@gmail.com>
2024-08-23 17:26:03 +0700

    chore: add LearningStateValue

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,14 +1,15 @@
 import { icons } from "lucide-react"
 
-type LearningState = {
+export type LearningStateValue = "wantToLearn" | "learning" | "learned"
+export type LearningState = {
 	label: string
 	value: string
 	icon: keyof typeof icons
 	className: string
 }
 
-export const LEARNING_STATES: LearningState[] = [
+export const LEARNING_STATES: ReadonlyArray<LearningState> = [
 	{ label: "To Learn", value: "wantToLearn", icon: "Bookmark", className: "text-foreground" },
 	{ label: "Learning", value: "learning", icon: "GraduationCap", className: "text-[#D29752]" },
 	{ label: "Learned", value: "learned", icon: "Check", className: "text-[#708F51]" }
-]
+] as const
