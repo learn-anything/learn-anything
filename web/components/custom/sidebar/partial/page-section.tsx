56c72a8a438c218994dccc8cbb0c1d86e9fb7489
Aslam H <iupin5212@gmail.com>
2024-08-24 09:04:49 +0700

    fix: collapsible page

--- web/components/custom/sidebar/partial/page-section.tsx
+++ web/components/custom/sidebar/partial/page-section.tsx
@@ -5,7 +5,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
 import { PlusIcon } from "lucide-react"
 import { cn, generateUniqueSlug } from "@/lib/utils"
-import { PersonalPage } from "@/lib/schema/personal-page"
+import { PersonalPage, PersonalPageLists } from "@/lib/schema/personal-page"
 import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
@@ -13,6 +13,9 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
 import { usePathname, useRouter } from "next/navigation"
 import Link from "next/link"
+import { LaIcon } from "../../la-icon"
+import { atomWithStorage } from "jotai/utils"
+import { useAtom } from "jotai"
 
 const createPageSchema = z.object({
 	title: z.string({ message: "Please enter a valid title" }).min(1, { message: "Please enter a valid title" })
@@ -20,55 +23,76 @@ const createPageSchema = z.object({
 
 type PageFormValues = z.infer<typeof createPageSchema>
 
+const isCollapsedAtom = atomWithStorage("isCollapsed", true)
+
 export const PageSection: React.FC = () => {
+	const [isCollapsed, setIsCollapsed] = useAtom(isCollapsedAtom)
+
+	const toggleCollapse = () => {
+		setIsCollapsed(!isCollapsed)
+	}
+
 	const { me } = useAccount({
 		root: { personalPages: [] }
 	})
-	const pathname = usePathname()
 
 	return (
 		<div className="group/pages flex flex-col gap-px py-2">
 			<div className="flex items-center gap-px">
 				<Button
 					variant="ghost"
-					className="group size-6 flex-1 items-center justify-start rounded-md px-2 py-1 focus:outline-0 focus:ring-0"
+					className="group size-6 flex-1 items-center justify-start rounded-md px-1.5 py-1 focus:outline-0 focus:ring-0"
+					onClick={toggleCollapse}
 				>
-					{/* <span className="mr-1.5 rotate-90 transition-transform duration-200">
-						<LaIcon name="ChevronRight" />
-					</span> */}
 					<p className="text-xs font-medium">Pages</p>
+					<span
+						className={cn("ml-1.5 transition-transform duration-200", {
+							"rotate-90": !isCollapsed
+						})}
+					>
+						<LaIcon name="ChevronRight" />
+					</span>
 				</Button>
 				<CreatePageForm />
 			</div>
-			<div className="flex flex-col gap-px">
-				{me?.root.personalPages.map(
-					page =>
-						page?.id && (
-							<div key={page.id} className="group/reorder-page relative">
-								<div className="group/sidebar-link relative flex min-w-0 flex-1">
-									<Link
-										href={`/pages/${page.id}`}
-										className={cn(
-											"group-hover/sidebar-link:bg-accent group-hover/sidebar-link:text-accent-foreground relative flex h-8 w-full items-center gap-2 rounded-md p-1.5 font-medium",
-											{ "bg-accent text-accent-foreground": pathname === `/pages/${page.id}` }
-										)}
-									>
-										<span className="flex max-w-[calc(100%-52px)] flex-1 items-center gap-1.5 truncate text-sm">
-											<span className="truncate opacity-60 group-hover/sidebar-link:opacity-100">{page.title}</span>
-										</span>
-									</Link>
-									{/* <Button
-								variant="ghost"
-								aria-label="Remove"
-								className="absolute right-0.5 top-1/2 z-[1] size-6 -translate-y-1/2 p-1 opacity-0 focus:opacity-100 group-hover/sidebar-link:opacity-100"
-							>
-								<LaIcon name="X" />
-							</Button> */}
-								</div>
+
+			{!isCollapsed && me?.root.personalPages && <PageList personalPages={me.root.personalPages} />}
+		</div>
+	)
+}
+
+const PageList: React.FC<{ personalPages: PersonalPageLists }> = ({ personalPages }) => {
+	const pathname = usePathname()
+
+	return (
+		<div className="flex flex-col gap-px">
+			{personalPages.map(
+				page =>
+					page?.id && (
+						<div key={page.id} className="group/reorder-page relative">
+							<div className="group/sidebar-link relative flex min-w-0 flex-1">
+								<Link
+									href={`/pages/${page.id}`}
+									className={cn(
+										"group-hover/sidebar-link:bg-accent group-hover/sidebar-link:text-accent-foreground relative flex h-8 w-full items-center gap-2 rounded-md p-1.5 font-medium",
+										{ "bg-accent text-accent-foreground": pathname === `/pages/${page.id}` }
+									)}
+								>
+									<span className="flex max-w-[calc(100%-52px)] flex-1 items-center gap-1.5 truncate text-sm">
+										<span className="truncate opacity-60 group-hover/sidebar-link:opacity-100">{page.title}</span>
+									</span>
+								</Link>
+								{/* <Button
+									variant="ghost"
+									aria-label="Remove"
+									className="absolute right-0.5 top-1/2 z-[1] size-6 -translate-y-1/2 p-1 opacity-0 focus:opacity-100 group-hover/sidebar-link:opacity-100"
+								>
+									<LaIcon name="X" />
+								</Button> */}
 							</div>
-						)
-				)}
-			</div>
+						</div>
+					)
+			)}
 		</div>
 	)
 }
@@ -119,8 +143,8 @@ const CreatePageForm: React.FC = () => {
 					variant="ghost"
 					aria-label="New Page"
 					className={cn(
-						"group flex size-6 items-center justify-center rounded-md p-0.5 focus:outline-0 focus:ring-0"
-						// 'opacity-0 group-hover/pages:opacity-100 data-[state="open"]:opacity-100'
+						"group flex size-6 items-center justify-center rounded-md p-0.5 focus:outline-0 focus:ring-0",
+						'opacity-0 group-hover/pages:opacity-100 data-[state="open"]:opacity-100'
 					)}
 				>
 					<PlusIcon size={16} />
