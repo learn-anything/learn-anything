1c006230c484f08e98770e3b537c9c8f3097bef0
marshennikovaolga <marshennikova@gmail.com>
2024-08-24 19:42:07 +0300

    topic button and count

--- web/components/custom/sidebar/partial/topic-section.tsx
+++ web/components/custom/sidebar/partial/topic-section.tsx
@@ -33,22 +33,39 @@ export const TopicSection = () => {
 		setSelectedStatus(prevStatus => (prevStatus === status ? null : status))
 	}
 
+	const topicCounts = {
+		"To Learn": 2,
+		Learning: 5,
+		Learned: 3,
+		get total() {
+			return this["To Learn"] + this.Learning + this.Learned
+		}
+	}
+
 	return (
 		<div className="space-y-1 overflow-hidden" ref={sectionRef}>
-			<div className="text-foreground flex w-full items-center justify-between rounded-md px-2 py-2 text-xs font-medium">
-				<span>
-					Topics <span className={getStatusColor(selectedStatus)}>{selectedStatus ? `: ${selectedStatus}` : ""}</span>
+			<div className="text-foreground group/topics flex w-full items-center justify-between rounded-md px-2 py-2 text-xs font-medium hover:bg-neutral-100">
+				<span className={getStatusColor(selectedStatus)}>
+					<span className="text-black dark:text-white">Topics</span>
+					{selectedStatus ? `: ${selectedStatus}` : ""}{" "}
+					{selectedStatus ? topicCounts[selectedStatus as keyof typeof topicCounts] : topicCounts.total} {/* </span> */}
 				</span>
+				<button className="opacity-0 transition-opacity duration-200 group-hover/topics:opacity-100">
+					<LaIcon name="Ellipsis" className="size-4 flex-shrink-0" />
+				</button>
 			</div>
 			<div>
 				{learningOptions.map(option => (
 					<Button
 						key={option.text}
 						onClick={() => statusSelect(option.text)}
-						className={`flex w-full items-center justify-start gap-2 rounded-md py-1 pl-1 text-sm font-medium hover:bg-neutral-100 dark:hover:bg-neutral-100/20 ${option.color} bg-inherit shadow-none`}
+						className={`flex w-full items-center justify-between rounded-md py-1 pl-1 text-sm font-medium hover:bg-neutral-100 dark:hover:bg-neutral-100/20 ${option.color} bg-inherit shadow-none`}
 					>
-						{option.icon && <span className={option.color}>{option.icon}</span>}
-						<span>{option.text}</span>
+						<div className="flex items-center gap-2">
+							{option.icon && <span className={option.color}>{option.icon}</span>}
+							<span>{option.text}</span>
+						</div>
+						<span className="option.color mr-2">{topicCounts[option.text as keyof typeof topicCounts]}</span>
 					</Button>
 				))}
 			</div>
