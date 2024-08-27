e38c10af6ba8b64d5c92f11bdd87ca5fd1f964ab
Nikita <github@nikiv.dev>
2024-08-27 16:08:47 +0300

    .

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,14 @@
 // not sure if good approach
-const TAURI_DEV = true
+const TAURI_DEV = false
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
 	output: TAURI_DEV ? "export" : undefined,
-	typescript: {
-		// TODO: hacky for tauri
-		ignoreBuildErrors: TAURI_DEV ? true : false
-	},
+	// typescript: {
+	// 	// TODO: hacky for tauri
+	// 	ignoreBuildErrors: TAURI_DEV ? true : false
+	// },
 	images: {
 		// TODO: hacky for tauri
 		unoptimized: TAURI_DEV ? true : false,
