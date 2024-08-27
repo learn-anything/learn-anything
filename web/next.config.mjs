ba92771c407950eba37f25f3c08711602f7de2c1
Nikita <github@nikiv.dev>
2024-08-27 15:00:29 +0300

    try change config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,7 +1,17 @@
+// not sure if good approach
+const TAURI_DEV = true
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	output: TAURI_DEV ? "export" : undefined,
+	typescript: {
+		// TODO: hacky for tauri
+		ignoreBuildErrors: TAURI_DEV ? true : false
+	},
 	images: {
+		// TODO: hacky for tauri
+		unoptimized: TAURI_DEV ? true : false,
 		remotePatterns: [
 			{
 				protocol: "https",
