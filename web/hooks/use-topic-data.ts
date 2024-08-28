f9c03a9b4d32154475a768ed9203d4d817308083
Aslam H <iupin5212@gmail.com>
2024-08-28 18:41:17 +0700

    chore: hook use topic data

--- web/hooks/use-topic-data.ts
+++ web/hooks/use-topic-data.ts
@@ -2,13 +2,16 @@ import { useMemo } from "react"
 import { useCoState } from "@/lib/providers/jazz-provider"
 import { PublicGlobalGroup } from "@/lib/schema/master/public-group"
 import { ID } from "jazz-tools"
+// import { Topic } from "@/lib/schema"
 
 export function useTopicData(topicName: string) {
-	const topics = useCoState(PublicGlobalGroup, process.env.NEXT_PUBLIC_JAZZ_GLOBAL_GROUP as ID<PublicGlobalGroup>, {
+	const group = useCoState(PublicGlobalGroup, process.env.NEXT_PUBLIC_JAZZ_GLOBAL_GROUP as ID<PublicGlobalGroup>, {
 		root: { topics: [] }
 	})
 
-	const topic = topics?.root.topics.find(topic => topic?.name === topicName)
+	// const topic = useCoState(Topic, "co_zS3TH4Lkj5MK9GEehinxhjjNTxB" as ID<Topic>, {})
+	const topic = group?.root.topics.find(topic => topic?.name === topicName)
+	console.log(topic?.id)
 
 	const allLinks = useMemo(() => {
 		if (topic?.latestGlobalGuide?.sections) {
