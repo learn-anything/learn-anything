18411864313105198a05c8b5716a7032dff96f87
Aslam H <iupin5212@gmail.com>
2024-08-28 18:40:39 +0700

    chore: set expand to false

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
-import { Inter } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/lib/providers/theme-provider"
 import "./globals.css"
@@ -32,7 +31,7 @@ export default function RootLayout({
 						<JotaiProvider>
 							<ConfirmProvider>
 								{children}
-								<Toaster />
+								<Toaster expand={false} />
 							</ConfirmProvider>
 						</JotaiProvider>
 					</ThemeProvider>
