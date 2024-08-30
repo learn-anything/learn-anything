e5bcc0acc119841a0fc3372cc309fd457b8bdc35
Damian Tarnawski <gthetarnav@gmail.com>
2024-08-30 12:20:13 +0200

    Make force graph work with server components

--- web/components/routes/PublicHomeRoute.tsx
+++ web/components/routes/PublicHomeRoute.tsx
@@ -1,18 +1,10 @@
-import * as react from "react"
-
-import type * as force_graph from "./force-graph-client"
-
-let graph_data_promise = import("./graph-data.json").then(a => a.default)
-let ForceGraphClient   = react.lazy(() => import("./force-graph-client"))
+import ForceGraph from "./force-graph-client"
 
 export default function PublicHomeRoute() {
-
-	let raw_graph_data = react.use(graph_data_promise)
-	
 	return (
 		<>
 			<h1>I want to learn</h1>
-			<ForceGraphClient raw_nodes={raw_graph_data} filter_query="" onNodeClick={() => {}}/>
+			<ForceGraph/>
 		</>
 	)
 }
