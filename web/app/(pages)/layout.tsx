0384f7d308f5f3d0de50f521159ff7c3ab515500
Damian Tarnawski <gthetarnav@gmail.com>
2024-08-30 12:26:30 +0200

    Bring back logged-in page

--- web/app/(pages)/layout.tsx
+++ web/app/(pages)/layout.tsx
@@ -5,18 +5,18 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 	// TODO: get it from jazz/clerk
 	const loggedIn = true
 
-	// if (loggedIn) {
-	// 	return (
-	// 		<div className="flex h-full min-h-full w-full flex-row items-stretch overflow-hidden">
-	// 			<Sidebar />
+	if (loggedIn) {
+		return (
+			<div className="flex h-full min-h-full w-full flex-row items-stretch overflow-hidden">
+				<Sidebar />
 
-	// 			<div className="flex min-w-0 flex-1 flex-col">
-	// 				<main className="relative flex flex-auto flex-col place-items-stretch overflow-auto lg:my-2 lg:mr-2 lg:rounded-md lg:border">
-	// 					{children}
-	// 				</main>
-	// 			</div>
-	// 		</div>
-	// 	)
-	// }
+				<div className="flex min-w-0 flex-1 flex-col">
+					<main className="relative flex flex-auto flex-col place-items-stretch overflow-auto lg:my-2 lg:mr-2 lg:rounded-md lg:border">
+						{children}
+					</main>
+				</div>
+			</div>
+		)
+	}
 	return <PublicHomeRoute />
 }
