5203bbb70a848a1462ae63c420127b86fb6bfeb6
Kisuyo <93681660+Kisuyo@users.noreply.github.com>
2024-08-30 14:58:02 +0300

    Command palette (#124)
    * command palette
    
    * .
    
    * add palette
    
    ---------
    
    Co-authored-by: Nikita <github@nikiv.dev>

--- web/app/(pages)/layout.tsx
+++ web/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import { SignedInClient } from "@/components/custom/clerk/signed-in-client"
 import { Sidebar } from "@/components/custom/sidebar/sidebar"
 import { PublicHomeRoute } from "@/components/routes/PublicHomeRoute"
+import { CommandPalette } from "@/components/ui/CommandPalette"
 import { JazzClerkAuth, JazzProvider } from "@/lib/providers/jazz-provider"
 import { currentUser } from "@clerk/nextjs/server"
 
@@ -17,6 +18,7 @@ export default async function PageLayout({ children }: { children: React.ReactNo
 				<JazzProvider>
 					<div className="flex h-full min-h-full w-full flex-row items-stretch overflow-hidden">
 						<Sidebar />
+						<CommandPalette />
 
 						<div className="flex min-w-0 flex-1 flex-col">
 							<main className="relative flex flex-auto flex-col place-items-stretch overflow-auto lg:my-2 lg:mr-2 lg:rounded-md lg:border">
