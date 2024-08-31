072287182ab22a3605342e7d1d9d7b62dbd1eaeb
Alice Carroll <git@alice-carroll.pet>
2024-09-01 02:38:29 +0300

    feat: add tauri app

---
+++ app/src/lib.rs
@@ -0,0 +1,4 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+	tauri::Builder::default().run(tauri::generate_context!()).expect("error while running tauri application");
+}
--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const isTauri = process.env.TAURI_ENV_DEBUG !== undefined;
+const isProd = process.env.NODE_ENV === 'production';
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
+const common = {
 	reactStrictMode: false,
 	images: {
 		remotePatterns: [
@@ -9,6 +14,22 @@ const nextConfig = {
 			}
 		]
 	}
-}
+};
+
+const config = {
+	web: common,
+	tauri: {
+		...common,
+		output: "export",
+		images: {
+			...common.images,
+			// Needed for Image components in SSG mode
+			unoptimized: true,
+		},
+		assetPrefix: isProd ? null : `http://${internalHost}:3000`,
+	},
+};
+
+const nextConfig = isTauri ? config.tauri : config.web;
 
 export default nextConfig
