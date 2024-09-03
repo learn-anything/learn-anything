1ceaffa5ad45a8edfd9b7004592f1e139475034a
Aslam H <iupin5212@gmail.com>
2024-09-03 21:13:16 +0700

    fix: undefined me

--- web/components/routes/link/partials/form/link-form.tsx
+++ web/components/routes/link/partials/form/link-form.tsx
@@ -135,7 +135,7 @@ export const LinkForm: React.FC<LinkFormProps> = ({
 	}
 
 	const onSubmit = (values: LinkFormValues) => {
-		if (isFetching) return
+		if (isFetching || !me) return
 
 		try {
 			const personalLinks = me.root?.personalLinks?.toJSON() || []
