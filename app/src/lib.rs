fa0c6bbf378b42b63cb4331cc815fa1452e8aeaa
Alice Carroll <git@alice-carroll.pet>
2024-09-03 22:24:26 +0300

    feat(tauri): add FS interface

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,4 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-	tauri::Builder::default().run(tauri::generate_context!()).expect("error while running tauri application");
+	tauri::Builder::default()
+		.plugin(tauri_plugin_fs::init())
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
