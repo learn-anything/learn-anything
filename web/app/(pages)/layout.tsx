cf04d5086c0a0ee6094316742d20c8817b3fa4d4
Anselm <anselm.eickhoff@gmail.com>
2024-09-05 18:41:51 +0100

    feat: Start using guest auth

--- web/app/(pages)/layout.tsx
+++ web/app/(pages)/layout.tsx
@@ -2,7 +2,7 @@ import { SignedInClient } from "@/components/custom/clerk/signed-in-client"
 import { Sidebar } from "@/components/custom/sidebar/sidebar"
 import { PublicHomeRoute } from "@/components/routes/PublicHomeRoute"
 import { CommandPalette } from "@/components/ui/CommandPalette"
-import { JazzClerkAuth, JazzProvider } from "@/lib/providers/jazz-provider"
+import { JazzAndAuth } from "@/lib/providers/jazz-provider"
 import { currentUser } from "@clerk/nextjs/server"
 
 export default async function PageLayout({ children }: { children: React.ReactNode }) {
@@ -13,21 +13,17 @@ export default async function PageLayout({ children }: { children: React.ReactNo
 	}
 
 	return (
-		<JazzClerkAuth>
-			<SignedInClient>
-				<JazzProvider>
-					<div className="flex h-full min-h-full w-full flex-row items-stretch overflow-hidden">
-						<Sidebar />
-						<CommandPalette />
+		<JazzAndAuth>
+			<div className="flex h-full min-h-full w-full flex-row items-stretch overflow-hidden">
+				<Sidebar />
+				<CommandPalette />
 
-						<div className="flex min-w-0 flex-1 flex-col">
-							<main className="relative flex flex-auto flex-col place-items-stretch overflow-auto lg:my-2 lg:mr-2 lg:rounded-md lg:border">
-								{children}
-							</main>
-						</div>
-					</div>
-				</JazzProvider>
-			</SignedInClient>
-		</JazzClerkAuth>
+				<div className="flex min-w-0 flex-1 flex-col">
+					<main className="relative flex flex-auto flex-col place-items-stretch overflow-auto lg:my-2 lg:mr-2 lg:rounded-md lg:border">
+						{children}
+					</main>
+				</div>
+			</div>
+		</JazzAndAuth>
 	)
 }
--- web/lib/providers/jazz-provider.tsx
+++ web/lib/providers/jazz-provider.tsx
@@ -4,7 +4,7 @@ import { createJazzReactApp } from "jazz-react"
 import { LaAccount } from "@/lib/schema"
 import { useClerk } from "@clerk/nextjs"
 import { createContext, useMemo, useState } from "react"
-import { AuthMethodCtx } from "jazz-react"
+import { useJazzClerkAuth } from "jazz-react-auth-clerk"
 
 const Jazz = createJazzReactApp({
 	AccountSchema: LaAccount
@@ -12,8 +12,21 @@ const Jazz = createJazzReactApp({
 
 export const { useAccount, useCoState, useAcceptInvite } = Jazz
 
-export function JazzProvider({ children }: { children: React.ReactNode }) {
-	return <Jazz.Provider peer="wss://mesh.jazz.tools/?key=example@gmail.com">{children}</Jazz.Provider>
+export function JazzAndAuth({ children }: { children: React.ReactNode }) {
+	const clerk = useClerk()
+
+	const [auth, state] = useJazzClerkAuth(clerk)
+
+	return (
+		<>
+			{state.errors.map((error) => (
+				<div key={error}>{error}</div>
+			))}
+			<Jazz.Provider auth={auth || "guest"} peer="wss://mesh.jazz.tools/?key=example@gmail.com">
+				{children}
+			</Jazz.Provider>
+		</>
+	)
 }
 
 export const JazzClerkAuthCtx = createContext<{
@@ -21,86 +34,3 @@ export const JazzClerkAuthCtx = createContext<{
 }>({
 	errors: []
 })
-
-export function JazzClerkAuth({ children }: { children: React.ReactNode }) {
-	const clerk = useClerk()
-	const [errors, setErrors] = useState<string[]>([])
-
-	const authMethod = useMemo(() => {
-		return new BrowserClerkAuth(
-			{
-				onError: error => {
-					void clerk.signOut()
-					setErrors(errors => [...errors, error.toString()])
-				}
-			},
-			clerk
-		)
-	}, [clerk])
-
-	return (
-		<JazzClerkAuthCtx.Provider value={{ errors }}>
-			<AuthMethodCtx.Provider value={authMethod}>{children}</AuthMethodCtx.Provider>
-		</JazzClerkAuthCtx.Provider>
-	)
-}
-
-import { Account, AuthMethod, AuthResult, ID } from "jazz-tools"
-import type { LoadedClerk } from "@clerk/types"
-import { AgentSecret } from "cojson"
-
-export class BrowserClerkAuth implements AuthMethod {
-	constructor(
-		public driver: BrowserClerkAuth.Driver,
-		private readonly clerkClient: LoadedClerk
-	) {}
-
-	async start(): Promise<AuthResult> {
-		if (this.clerkClient.user) {
-			const storedCredentials = this.clerkClient.user.unsafeMetadata
-			if (storedCredentials.jazzAccountID) {
-				if (!storedCredentials.jazzAccountSecret) {
-					throw new Error("No secret for existing user")
-				}
-				return {
-					type: "existing",
-					credentials: {
-						accountID: storedCredentials.jazzAccountID as ID<Account>,
-						secret: storedCredentials.jazzAccountSecret as AgentSecret
-					},
-					onSuccess: () => {},
-					onError: (error: string | Error) => {
-						this.driver.onError(error)
-					}
-				}
-			} else {
-				return {
-					type: "new",
-					creationProps: {
-						name: this.clerkClient.user.fullName || this.clerkClient.user.username || this.clerkClient.user.id
-					},
-					saveCredentials: async (credentials: { accountID: ID<Account>; secret: AgentSecret }) => {
-						await this.clerkClient.user?.update({
-							unsafeMetadata: {
-								jazzAccountID: credentials.accountID,
-								jazzAccountSecret: credentials.secret
-							}
-						})
-					},
-					onSuccess: () => {},
-					onError: (error: string | Error) => {
-						this.driver.onError(error)
-					}
-				}
-			}
-		} else {
-			throw new Error("Not signed in")
-		}
-	}
-}
-
-export namespace BrowserClerkAuth {
-	export interface Driver {
-		onError: (error: string | Error) => void
-	}
-}
