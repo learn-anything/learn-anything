46401c12fd5836bbab145ef3b0996c622fa001e5
Aslam H <iupin5212@gmail.com>
2024-09-07 01:45:35 +0700

    feat(util): suffle array

--- web/lib/utils/index.ts
+++ web/lib/utils/index.ts
@@ -25,6 +25,15 @@ export const searchSafeRegExp = (inputValue: string) => {
 	return new RegExp(escapedChars.join(".*"), "i")
 }
 
+export function shuffleArray<T>(array: T[]): T[] {
+	const shuffled = [...array]
+	for (let i = shuffled.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+	}
+	return shuffled
+}
+
 export * from "./urls"
 export * from "./slug"
 export * from "./keyboard"
