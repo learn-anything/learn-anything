e19a5dedf31ae60bec07aaa0726f8e36400a0f50
Nikita <github@nikiv.dev>
2024-09-06 13:23:36 +0300

    .

--- api/src/api.ts
+++ api/src/api.ts
@@ -1,4 +1,4 @@
-import { api } from "encore.dev/api"
+import { api, APIError } from "encore.dev/api"
 import { startWorker } from "jazz-nodejs"
 import { ID } from "jazz-tools"
 
@@ -9,11 +9,45 @@ const JAZZ_PUBLIC_GLOBAL_GROUP = process.env.JAZZ_PUBLIC_GLOBAL_GROUP
 // return all content for GlobalTopic
 export const get = api(
 	{ expose: true, method: "GET", path: "/topic/:topic" },
-	async ({ topic }: { topic: string }): Promise<Response> => {
+	async ({
+		topic
+	}: {
+		topic: string
+		// TODO: can return type be inferred like Elysia?
+	}): Promise<{
+		links: {
+			label: string
+			url: string
+		}[]
+	}> => {
 		const { worker } = await startWorker({
 			accountID: "co_zhvp7ryXJzDvQagX61F6RCZFJB9",
 			accountSecret: JAZZ_WORKER_SECRET
 		})
+
+		// TODO: how to get the import from outside this package?
+		const globalGroupId = process.env.JAZZ_PUBLIC_GLOBAL_GROUP as ID<any>
+		const globalGroup = await PublicGlobalGroup.load(globalGroupId, worker, {
+			root: {
+				topics: [
+					{
+						latestGlobalGuide: {
+							sections: [
+								{
+									links: [{}]
+								}
+							]
+						}
+					}
+				],
+				forceGraphs: [
+					{
+						connections: [{}]
+					}
+				]
+			}
+		})
+		if (!globalGroup) throw APIError.notFound
 
 		// const globalGroupId = process.env.JAZZ_PUBLIC_GLOBAL_GROUP as ID<any>
 		// console.log(globalGroupId)
@@ -26,10 +60,3 @@ export const get = api(
 		return topicContent
 	}
 )
-
-interface Response {
-	links: {
-		label: string
-		url: string
-	}[]
-}
