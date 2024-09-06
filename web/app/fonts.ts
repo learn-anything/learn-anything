60ff8f777cfc5250d823602c20958912fdd1d364
Aslam H <iupin5212@gmail.com>
2024-09-07 01:43:51 +0700

    feat: new fonts file

---
+++ web/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Raleway } from "next/font/google"
+export { GeistSans } from "geist/font/sans"
+export { GeistMono } from "geist/font/mono"
+// import { Inter } from "next/font/google"
+
+// export const inter = Inter({ subsets: ["latin"] })
+export const raleway = Raleway({ subsets: ["latin"] })
