af1599230335daa30eec7911cb4ab23c3ec26ee6
Aslam H <iupin5212@gmail.com>
2024-09-07 01:44:05 +0700

    chore: apply geist

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next"
-import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/lib/providers/theme-provider"
 import "./globals.css"
-
 import { ClerkProviderClient } from "@/components/custom/clerk/clerk-provider-client"
 import { JotaiProvider } from "@/lib/providers/jotai-provider"
 import { Toaster } from "@/components/ui/sonner"
 import { ConfirmProvider } from "@/lib/providers/confirm-provider"
 import { DeepLinkProvider } from "@/lib/providers/deep-link-provider"
-
-const fontSans = FontSans({
-	subsets: ["latin"],
-	variable: "--font-sans"
-})
+import { GeistMono, GeistSans } from "./fonts"
 
 export const metadata: Metadata = {
 	title: "Learn Anything",
@@ -29,7 +23,7 @@ export default function RootLayout({
 		<html lang="en" className="h-full w-full" suppressHydrationWarning>
 			<ClerkProviderClient>
 				<DeepLinkProvider>
-					<body className={cn("h-full w-full font-sans antialiased", fontSans.variable)}>
+					<body className={cn("h-full w-full font-sans antialiased", GeistSans.variable, GeistMono.variable)}>
 						<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 							<JotaiProvider>
 								<ConfirmProvider>
