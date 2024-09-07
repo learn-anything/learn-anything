bf6a374bebe271f3d78a6e6ad66d0583c7d391c0
Kisuyo <ig.intr3st@gmail.com>
2024-09-07 19:50:36 +0200

    try

--- web/components/routes/public/PublicHomeRoute.tsx
+++ web/components/routes/public/PublicHomeRoute.tsx
@@ -7,6 +7,7 @@ import { Autocomplete } from "./Autocomplete"
 import { useRouter } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { raleway } from "@/app/fonts"
+import TextBlurTransition from "@/components/custom/text-blur-transition"
 
 let graph_data_promise = import("./graph-data.json").then(a => a.default)
 
@@ -37,14 +38,25 @@ export function PublicHomeRoute() {
 
 			<div className="absolute left-1/2 top-1/2 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 transform max-sm:px-5">
 				<motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
-					<motion.h1
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ delay: 0.5, duration: 1 }}
+						className="absolute left-1/2 top-1/2 h-[350px] w-[500px] -translate-x-1/2 -translate-y-1/2 transform md:h-[450px] md:w-[700px]"
+						style={{
+							background:
+								"radial-gradient(ellipse 80% 60% at 50% 50%, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.5) 40%, rgba(0, 0, 0, 0) 70%)",
+							backgroundSize: "100% 100%",
+							backgroundPosition: "center",
+							backgroundRepeat: "no-repeat",
+							zIndex: 0
+						}}
+					></motion.div>
+					<TextBlurTransition
 						className={cn("mb-2 text-center text-5xl font-bold tracking-tight sm:mb-4 md:text-7xl", raleway.className)}
-						initial={{ opacity: 0, y: -20 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ duration: 0.5, delay: 0.2 }}
 					>
 						I want to learn
-					</motion.h1>
+					</TextBlurTransition>
 					<Autocomplete topics={raw_graph_data} onSelect={handleTopicSelect} onInputChange={handleInputChange} />
 				</motion.div>
 			</div>
