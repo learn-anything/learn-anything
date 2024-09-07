c87ec974ef8683ffafd39be080a448cb83a4cbbe
Aslam H <iupin5212@gmail.com>
2024-09-08 05:41:43 +0700

    chore: jest use env

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "jest"
 import nextJest from "next/jest.js"
+import dotenv from "dotenv"
 
 const createJestConfig = nextJest({
 	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
 	dir: "./"
 })
+
+dotenv.config({ path: ".env.local" })
 
 // Add any custom config to be passed to Jest
 const config: Config = {
@@ -13,7 +16,8 @@ const config: Config = {
 	// Add more setup options before each test is run
 	// setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 	// Automatically clear mock calls, instances, contexts and results before every test
-	clearMocks: true
+	clearMocks: true,
+	moduleDirectories: ["node_modules", __dirname]
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
