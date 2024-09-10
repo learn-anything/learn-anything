423dc920d954509d4855bf70277de953068e937d
Nikita <github@nikiv.dev>
2024-09-10 14:09:32 +0300

    no edge sentry

--- web/instrumentation.ts
+++ web/instrumentation.ts
@@ -2,8 +2,4 @@ export async function register() {
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		await import("./sentry.server.config")
 	}
-
-	if (process.env.NEXT_RUNTIME === "edge") {
-		await import("./sentry.edge.config")
-	}
 }
