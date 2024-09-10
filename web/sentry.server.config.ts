9e28aa5995ea87a63709dbbc3ac064e294783bfe
Nikita <github@nikiv.dev>
2024-09-10 14:18:37 +0300

    Sentry (#157)
    * .
    
    * add sentry
    
    * prettier
    
    * up packages
    
    * no edge sentry
    
    ---------
    
    Co-authored-by: Aslam H <iupin5212@gmail.com>

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs"
+
+Sentry.init({
+	dsn: "https://643e210c090ea97f641b7f5fb253f16b@o301266.ingest.us.sentry.io/4507927630118912",
+
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+	tracesSampleRate: 1,
+
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false
+})
