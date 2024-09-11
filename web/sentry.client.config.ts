8d2cf9e676f72479a74a8745c8d5e56ac10e145d
Aslam H <iupin5212@gmail.com>
2024-09-11 14:07:45 +0700

    fix(sntry): sentry client error report

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-	dsn: process.env.SENTRY_DSN,
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
 	// Add optional integrations for additional features
 	integrations: [Sentry.replayIntegration()],
--- web/sentry.server.config.ts
+++ web/sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-	dsn: process.env.SENTRY_DSN,
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
 	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
 	tracesSampleRate: 1,
