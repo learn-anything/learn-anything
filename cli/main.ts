0373cfc9c33c8b2408e2c0c41fe963c3e739f381
Nikita <github@nikiv.dev>
2024-09-22 14:45:11 +0300

    .

--- cli/main.ts
+++ cli/main.ts
@@ -1,15 +1,12 @@
 import { Command } from "@effect/cli"
-import { NodeContext, NodeRuntime } from "@effect/platform-node"
+import { BunContext } from "@effect/platform-bun"
 import { Console, Effect } from "effect"
 
-// Define the top-level command
-const command = Command.make("hello-world", {}, () => Console.log("Hello World"))
+const command = Command.make("test", {}, () => Console.log("test"))
 
-// Set up the CLI application
 const cli = Command.run(command, {
-	name: "Hello World CLI",
-	version: "v1.0.0"
+	name: "learn-anything.xyz CLI",
+	version: "v0.0.1"
 })
 
-// Prepare and run the CLI application
-cli(process.argv).pipe(Effect.provide(NodeContext.layer), NodeRuntime.runMain)
+Effect.runPromise(cli(process.argv).pipe(Effect.provide(BunContext.layer)))
